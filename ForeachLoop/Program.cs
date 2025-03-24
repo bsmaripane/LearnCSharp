a03000fe77f290aabf9a9da6c18a16a8d1690197
Belmy Maripane <bsmaripane@gmail.com>
2025-03-24 19:27:42 +0200

    24/03/2025 Foreach loop

---
+++ ForeachLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6, 7};
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+        }
+    }
+}
