7c1c4eed78fbbc4e8064ba781c5452ad49d765fe
Belmy Maripane <bsmaripane@gmail.com>
2025-03-26 11:51:49 +0200

    Unintentional Update

--- ForeachLoop/Program.cs
+++ ForeachLoop/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6, 7};
 
