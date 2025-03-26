3f1859c8f341ef16e8c6e5bf342a5c7d8aa8e493
Belmy Maripane <bsmaripane@gmail.com>
2025-03-26 12:29:36 +0200

    26/032025 Read and display user input

---
+++ UserInputAndDataTypeString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UserInputAndDataTypeString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Addition Calculator\n\n");
+
+            Console.WriteLine("Enter something: ");
+
+            // Takes the user input and stores it
+            // Variable with the name "userInput" and the data type "string"
+            // strings are used for text
+            string userInput = Console.ReadLine();
+            Console.WriteLine("You entered " + userInput);
+
+            Console.ReadKey();
+        }
+    }
+}
+
