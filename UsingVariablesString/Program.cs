765963b8cd9787680ce75c5c9132fff792c6b2aa
Belmy Maripane <bsmaripane@gmail.com>
2025-03-26 12:04:30 +0200

    26/03/2025 Declare variable, assign a string and display the string

---
+++ UsingVariablesString/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UsingVariablesString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C# Programming MasterClass!\n\n");
+
+            // Declare a variable of type string
+            string myGirlFriendsName;
+
+            // Assign a string
+            myGirlFriendsName = "Felicia";
+
+            // Display the string store in myGirlFriendName
+            Console.WriteLine(myGirlFriendsName);
+
+            Console.ReadKey();
+        }
+    }
+}
