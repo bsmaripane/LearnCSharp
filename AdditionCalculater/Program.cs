73379ab522b6f9ba6d45458d744ad03eb8054fa2
Belmy Maripane <bsmaripane@gmail.com>
2025-03-27 07:47:19 +0200

    27/03/2025 AdditionalCalculator

---
+++ AdditionCalculater/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AdditionCalculater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialise variables
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int sum = 0;
+
+            // Prompt user for input of two integers
+            Console.WriteLine("Enter the first number: ");
+            firstNumber = Console.Read();
+
+            Console.WriteLine("Enter the second number: ");
+            secondNumber = Console.Read();
+
+            // Compute the sum
+            sum = firstNumber + secondNumber;
+
+            // Display the sum
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
