e96039eee715839c432efe8367b14d4a00c03ec6
Belmy Maripane <bsmaripane@gmail.com>
2025-03-31 13:51:00 +0200

    31/03/2025 Explicit conversion

---
+++ ExplicitConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿// Explicit Conversion          
+namespace ExplicitConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long myLong = 123451815116198;
+
+            int myInt = (int)myLong;
+
+            Console.WriteLine(myInt);
+
+            float myFloat = 2458.254f;
+            double myDouble = 1943.987218151891;
+            Console.WriteLine(myDouble);
+
+            myFloat = (float)myDouble;
+            Console.WriteLine(myFloat);
+
+            myInt = (int)myDouble;
+            Console.WriteLine(myInt);
+
+            Console.ReadKey();
+        }
+    }
+}
