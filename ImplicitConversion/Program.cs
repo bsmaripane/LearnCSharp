04ea26b16e87319a940897dea9d46acd1f523755
Belmy Maripane <bsmaripane@gmail.com>
2025-03-31 10:11:11 +0200

    31/03/2025 implicit conversion

---
+++ ImplicitConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿// Implicit convertion
+namespace ImplicitConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 24744944589;
+            double myDouble = myInt;       // implicit conversion
+
+            long myLong = myInt;    // implicit conversion
+
+            float myFloat = myInt;
+
+            myFloat = 15.91548f;
+
+            myDouble = myFloat;
+
+            // Wait for the key press before closing
+            Console.ReadKey();
+        }
+    }
+}
