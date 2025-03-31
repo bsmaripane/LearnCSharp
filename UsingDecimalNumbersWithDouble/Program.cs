064ac9b9e56699a4c17baa2cf0ee4f95940411ae
Belmy Maripane <bsmaripane@gmail.com>
2025-03-31 09:00:03 +0200

    31/03/2025 using decimal numbers instead with double

---
+++ UsingDecimalNumbersWithDouble/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace UsingDecimalNumbersWithDouble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and initialise variables
+            double firstNumber = 0.0;
+            double secondNumber = 0.0;
+            double sum = 0.0;
+
+            // Prompt user for input of two integers
+            Console.WriteLine("Enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            // Compute the sum
+            sum = firstNumber + secondNumber;
+
+            // Display the sum
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
