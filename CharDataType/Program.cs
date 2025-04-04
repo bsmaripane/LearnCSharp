d675bf4f980b4ed0a45a65c304d2b938987e8871
Belmy Maripane <bsmaripane@gmail.com>
2025-04-04 05:45:06 +0200

    04/04/2025 char data type

---
+++ CharDataType/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CharDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //char myFavoriteCharacter = '😊';
+            string smileyFace = "😊";
+
+            Console.WriteLine(smileyFace);
+
+            // wait for key press before closing
+            Console.ReadKey();
+        }
+    }
+}
