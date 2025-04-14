2e92bddbee3d637d57a0cacf0e7d96464aff8da7
Belmy Maripane <bsmaripane@gmail.com>
2025-04-14 09:44:00 +0200

    14/04/2025 Declaring and initialsing a bool variable

---
+++ DataTypeBool/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DataTypeBool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRainy = false;
+            bool hasUnbrella = true;
+
+
+        }
+    }
+}
