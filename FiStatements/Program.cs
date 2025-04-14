ddced785e6646d9bcfbc96daa33cea2cb0faf709
Belmy Maripane <bsmaripane@gmail.com>
2025-04-14 10:03:05 +0200

    14/04/2025 Basic if statements

---
+++ FiStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FiStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRainy = false;
+            bool hasUmbrella = true;
+
+            if (!isRainy)
+            {
+                Console.WriteLine("It is raining!");
+
+                if (!hasUmbrella)
+                    Console.WriteLine("Lucky me, I have an Umbrella.");
+                else
+                    Console.WriteLine("Sorry for me, I don't have an Umbrella.");
+            }
+            else
+            {
+                Console.WriteLine("It is not raining!");
+            }
+        }
+    }
+}
