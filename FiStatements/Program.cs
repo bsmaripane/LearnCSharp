cbb7cf0cde2ddc3ff11ec9716babe82a6953db7c
Belmy Maripane <bsmaripane@gmail.com>
2025-04-15 04:52:11 +0200

    14/04/2025 if statement test

--- FiStatements/Program.cs
+++ FiStatements/Program.cs
@@ -6,17 +6,18 @@
         {
             bool isRainy = false;
             bool hasUmbrella = true;
+            bool isHeavyRain = true;
 
             // Logical Operators
             // AND  &&
             // OR   ||
             // NOT  !
 
-            if (!isRainy)
+            if (isRainy || !isHeavyRain)
             {
                 Console.WriteLine("It is raining!");
 
-                if (!hasUmbrella)
+                if (hasUmbrella && isHeavyRain)
                     Console.WriteLine("Lucky me, I have an Umbrella.");
                 else
                     Console.WriteLine("Sorry for me, I don't have an Umbrella.");
