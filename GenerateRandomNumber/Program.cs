0612eed7ddb80abcf3ee7573ebf33d53f4b4da53
Belmy Maripane <bsmaripane@gmail.com>
2025-05-07 13:51:15 +0200

    07/05/2025 generate random number

---
+++ GenerateRandomNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenerateRandomNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generate a random number!\n");
+
+            Console.ReadKey();
+        }
+    }
+}
