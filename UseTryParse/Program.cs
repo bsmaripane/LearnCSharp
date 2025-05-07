6e8582fd8e5371f3aba6d98c1bdd0655121150c3
Belmy Maripane <bsmaripane@gmail.com>
2025-05-07 12:42:06 +0200

    07/05/2025 TryParse to ensure the app don't crash if a number  is not entered

---
+++ UseTryParse/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UseTryParse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number");
+            string inputString = Console.ReadLine();
+            int num1;
+            //int num1 = int.Parse(inputString);
+            bool isNumber = int.TryParse(inputString, out num1);
+
+            if (isNumber)
+                Console.WriteLine("Well done, you entered a number");
+            else
+                Console.WriteLine("Haha you troll, You should've entered a number");
+
+            num1++;
+            Console.WriteLine("User entered number +1 " + num1);
+
+            Console.ReadKey();
+        }
+    }
+}
