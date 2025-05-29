277d1f832f3a340183d837680b65210e406e6e87
Belmy Maripane <bsmaripane@gmail.com>
2025-05-29 08:18:53 +0200

    29/05/2025 generate number and else if statement to check if

--- GenerateRandomNumber/Program.cs
+++ GenerateRandomNumber/Program.cs
@@ -6,6 +6,40 @@
         {
             Console.WriteLine("Generate a random number!\n");
 
+            // Creating an instance of the Random Class
+            Random random = new Random();
+
+            // Generate random number between 1 and 50
+            int randomNumber = random.Next(1, 50);
+
+            Console.WriteLine("Guess thr number");
+            string inputString = Console.ReadLine();
+
+            int num1 = 0;
+            bool isNumber = int.TryParse(inputString, out num1);
+
+            if (isNumber)
+            {
+                Console.WriteLine("Well done, you have entered a number");
+
+                if (num1 == randomNumber)
+                {
+                    Console.WriteLine("Wow, great guess");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, guessed wrong!");
+                }
+
+                Console.WriteLine($"\nRandom number: {randomNumber} and entered number: {num1}");
+            }
+            else
+            {
+                Console.WriteLine("Haha you troil. You shoud've entered a number");
+            }
+
+            
+
             Console.ReadKey();
         }
     }
