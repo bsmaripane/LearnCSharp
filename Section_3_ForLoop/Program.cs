825487c3c009d7a8de4b558fbb00c9fb4f000ff8
Belmy Maripane <bsmaripane@gmail.com>
2025-09-22 09:55:31 +0200

    Basic for loop

---
+++ Section_3_ForLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Section_3_ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"I is {i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
