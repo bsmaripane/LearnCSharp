c23a1b1d6d192b19617f35de8d5676fff29c345c
Belmy Maripane <bsmaripane@gmail.com>
2025-09-22 10:15:51 +0200

    Countdown from 10 to 0, and breakout of the loop if count = 0

--- Section_3_ForLoop/Program.cs
+++ Section_3_ForLoop/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            for (int counter = 10; counter >= 0; counter--)
+            for (int counter = 10; counter != 0; counter--)
             {
                 Console.WriteLine("Counter is " + counter);
             }
