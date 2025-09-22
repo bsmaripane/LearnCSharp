e754d9d4e1c4f9f27d4aad7f6a48f91101842a3a
Belmy Maripane <bsmaripane@gmail.com>
2025-09-22 11:22:31 +0200

    Added thread.sleep() to pause the counter for 2000ms for each loop

--- Section_3_ForLoop/Program.cs
+++ Section_3_ForLoop/Program.cs
@@ -7,6 +7,7 @@
             for (int counter = 10; counter != 0; counter--)
             {
                 Console.WriteLine("Counter is " + counter);
+                Thread.Sleep(2000); // pause for 2000 ms
             }
             Console.ReadKey();
         }
