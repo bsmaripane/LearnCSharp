205b6d6d469570d8bb1b31a0d2966cc2e319b6f5
Belmy Maripane <bsmaripane@gmail.com>
2025-09-22 11:44:01 +0200

    A string represents the rocket for  Rocket Landing Simulation

---
+++ Challenge_Rocket_Landing_Simulation/Program.cs
@@ -0,0 +1,24 @@
+﻿// Challenge: Rocket Landing Simulation
+// Create a fun and interactive console application in C# that simulates a rocket landing.
+namespace Challenge_Rocket_Landing_Simulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // String that represents the rocket
+            string rocket = """
+                    /\
+                   /  \
+                   |  |
+                   |  |
+                  /    \
+                  |/\/\|
+
+                """;
+
+            Console.WriteLine(rocket);
+            Console.ReadKey();
+        }
+    }
+}
