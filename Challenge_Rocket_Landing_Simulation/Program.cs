862903c62fabda5666908b61af8d080d59d012fb
Belmy Maripane <bsmaripane@gmail.com>
2025-09-22 12:25:59 +0200

    Descent mechanism on the console Repeating the newline character each iteration

--- Challenge_Rocket_Landing_Simulation/Program.cs
+++ Challenge_Rocket_Landing_Simulation/Program.cs
@@ -17,12 +17,15 @@ namespace Challenge_Rocket_Landing_Simulation
                    \  /
                     \/
                 """;
-
+            int num = 0;
             for (int launch = 10; launch >= 0; launch--)
-            {
+            {          
+                string repeatNewLine = new string('\n', num + 1);
+                Console.WriteLine(repeatNewLine);
                 Console.WriteLine("\n" + rocket_landing + "\t\t\t" + (launch * 10) + " meters");
                 Thread.Sleep(1500);
                 Console.Clear();
+                num++;
             }
             Console.ReadKey();
         }
