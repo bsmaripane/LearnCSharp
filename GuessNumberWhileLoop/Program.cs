e335c7b94ad73dbf9628e379cf64e6116d9c40e6
Belmy Maripane <bsmaripane@gmail.com>
2025-10-06 08:52:57 +0200

    Add program to guess a number using a while loop and if-else statements.

---
+++ GuessNumberWhileLoop/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace GuessNumberWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 53;
+            int guessedNumber = 0;
+            bool isValid = true;
+            int counter = 0;
+
+
+            while (isValid)
+            {
+                ++counter;
+                Console.WriteLine($"Number of Tries: {counter}");
+
+                Console.Write("Guess the number: ");
+                bool isNumber = int.TryParse( Console.ReadLine(), out guessedNumber );
+
+                if (isNumber)
+                {
+                    if (guessedNumber < number)
+                        Console.WriteLine("Too low! Try again.");
+                    else if (guessedNumber > number)
+                        Console.WriteLine("Too high! Tyr again.");
+                    else
+                    {
+                        Console.WriteLine($"\n\nCongratulations! You gessed the number right! It took you {counter} tries");
+                        isValid = false;
+                    }
+                }
+                else
+                    Console.WriteLine($"\n\nError: {guessedNumber} is not a valid number! try again!");
+
+                if (counter == 5)
+                {
+                    Console.WriteLine($"\n\nFailed the guess number! \nMaximum of {counter} tries reached.");
+                    isValid = false;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
