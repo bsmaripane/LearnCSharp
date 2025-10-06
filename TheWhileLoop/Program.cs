aed1fe427a3c306fa54d8faccd64a0c91be798d2
Belmy Maripane <bsmaripane@gmail.com>
2025-10-06 08:06:50 +0200

    The counter up to 29 using a while loop

---
+++ TheWhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TheWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            while (counter < 30)
+            {
+                Console.WriteLine($"The counter is {counter}");
+                counter++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
