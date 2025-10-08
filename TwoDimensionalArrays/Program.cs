f0543f1fad264045076da7e86312a37748df9a77
Belmy Maripane <bsmaripane@gmail.com>
2025-10-08 14:43:11 +0200

    Declaration of 2D and 3D array and declare and initialise 2D array and print keypad.

---
+++ TwoDimensionalArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TwoDimensionalArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2DDeclaration = new int[3, 3];
+            
+            int[,,] array3DDeclaration = new int[3, 3, 4];
+
+            int[,] array2DInitialised = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                    Console.Write(array2DInitialised[row, col] + " ");
+                Console.WriteLine();
+                if (row == 2)
+                    Console.WriteLine("# " + 0 + " * ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
