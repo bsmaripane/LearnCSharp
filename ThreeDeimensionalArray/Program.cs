d88105f0761194d26dca2d9c32c51375b664ff27
Belmy Maripane <bsmaripane@gmail.com>
2025-10-08 15:20:34 +0200

    Declare and initialise 3D array and print it.

---
+++ ThreeDeimensionalArray/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ThreeDeimensionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,,] simple3DArray =
+            {
+                {
+                    { "000", "001", "002"},
+                    { "010", "011", "012"},
+                    { "020", "021", "022"}
+                },
+                {
+                    { "100", "101", "102"},
+                    { "110", "111", "112"},
+                    { "120", "121", "121"}
+                },
+                {
+                    { "200", "201", "201"},
+                    { "210", "211", "212"},
+                    { "220", "221", "222"}
+                }
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int f = 0; f < 3; f++)
+                        Console.Write(simple3DArray[i, j, f] + " ");
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
