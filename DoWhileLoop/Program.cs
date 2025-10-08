6d9134df9982e40b392150f6953a12e243d01d17
Belmy Maripane <bsmaripane@gmail.com>
2025-10-08 11:48:15 +0200

    Add a do-while loop to calculate the average.

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DoWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentScore;
+            int sum = 0;
+            int counter = 0;
+            do
+            {
+                Console.WriteLine("Enter your student score. Enter -1 to finish!");
+                currentScore = int.Parse(Console.ReadLine());
+
+                if (currentScore != -1)
+                {
+                    sum += currentScore;
+                    counter++;
+                }
+            }
+            while (currentScore != -1);
+
+            int averageScore = sum / counter;
+            Console.WriteLine($"The student average score is {averageScore}");
+
+            Console.ReadKey();
+        }
+    }
+}
