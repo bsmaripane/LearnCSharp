8eb3b3d2ac8ed69cae0d2c02ef9b5295cc62a48a
Belmy Maripane <bsmaripane@gmail.com>
2025-10-26 18:09:11 +0200

    MyMethod() is used to print "MyMethod was called"

---
+++ CallMethod/CallMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CallMethod
+{
+    internal class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("MyMethod was called.");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Call a method!");
+            MyMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
