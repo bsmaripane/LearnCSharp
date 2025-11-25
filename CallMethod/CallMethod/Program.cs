aa4f9092891f0e37c46130174b11e80e985cedfe
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 08:06:17 +0200

    Passing a paramether to void method.

--- CallMethod/CallMethod/Program.cs
+++ CallMethod/CallMethod/Program.cs
@@ -6,10 +6,16 @@
         {
             Console.WriteLine("MyMethod was called.");
         }
+
+        static void WriteSomething(string something)
+        {
+            Console.WriteLine($"This argument is passed to the method: {something}");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Call a method!");
             MyMethod();
+            WriteSomething("Today is a beautiful day!");
 
             Console.ReadKey();
         }
