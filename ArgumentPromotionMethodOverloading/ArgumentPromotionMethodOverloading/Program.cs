3724a1675bf99513d3625f1a0b7c2448806a30e6
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 10:15:47 +0200

    Argument promotion in overloading methods

---
+++ ArgumentPromotionMethodOverloading/ArgumentPromotionMethodOverloading/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ArgumentPromotionMethodOverloading
+{
+    internal class Program
+    {
+        static void PrintValue(int value)
+        {
+            Console.WriteLine($"Integer value: {value}");
+        }
+        static void PrintValue(string value)
+        {
+            Console.WriteLine($"String value: {value}");
+        }
+
+        static void PrintValue(object value)
+        {
+            Console.WriteLine($"Object value: {value}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argument Promotion in Method Overloading");
+
+            int intValue = 30;
+            string strValue = "Belmy Semape Maripane!";
+            double doubleValue = 11.67;
+            float floatValue = 5.89f;
+
+            PrintValue(intValue);        // Calls PrintValue(int)
+            PrintValue(strValue);      // Calls PrintValue(string)
+            PrintValue(doubleValue);   // Calls PrintValue(object) due to argument promotion
+            PrintValue(floatValue);    // Calls PrintValue(object) due to argument promotion
+
+            Console.ReadKey();
+        }
+    }
+}
