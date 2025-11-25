eea1b2b710fbc490544347ad2117eeb583e557b6
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 14:29:09 +0200

    Simple example of recursion function print numbers in descending order.

---
+++ RecursionFunction/RecursionFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RecursionFunction
+{
+    internal class Program
+    {
+        static void CountDown(int number)
+        {
+            // Base case
+            if (number == 0)
+            {
+                Console.WriteLine("Bast off!");
+                return;
+            }
+
+            Console.WriteLine(number);
+
+            // Recursive call
+            CountDown(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recursion Function in C#!");
+
+            var age = 30;
+
+            CountDown(age);
+
+            Console.ReadKey(true);
+        }
+    }
+}
