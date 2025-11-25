c56c266b23b156f87d9beb1f2fa13cdefb2ea44c
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 11:49:09 +0200

    out parameter modifer, method assign the value before returning the parameter.

--- ParametersModifiers/ParametersModifiers/Program.cs
+++ ParametersModifiers/ParametersModifiers/Program.cs
@@ -6,15 +6,25 @@
         {
             num += 10;
         }
+
+        static void GetValue(out int age)
+        {
+            age = 30;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Parameter Modifers in C#!");
 
             int number = 5;
+            int age;
 
             Console.WriteLine($"Before ModifyValues: {number}");
             ModifyValues(ref number);
             Console.WriteLine($"After ModifyValues: {number}");
+
+            GetValue(out age);
+            Console.WriteLine($"Age from GetValue: {age}");
 
             Console.ReadKey();
         }
