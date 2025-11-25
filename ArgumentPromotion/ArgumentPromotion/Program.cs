ebdd755ab0a3002c9d6d3e662d354466ba466b62
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 10:04:28 +0200

    Argument Promotion C# automatically promote argument from smaller data type tolarger data type.

---
+++ ArgumentPromotion/ArgumentPromotion/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ArgumentPromotion
+{
+    internal class Program
+    {
+        static void DisplayNumber(double number)
+        {
+            Console.WriteLine($"The number is: {number}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argument Promotion C# automatically promote argument from smaller to larger data type!");
+            int smallInt = 10;
+
+            // Implicitly promote int to double
+            DisplayNumber(smallInt); // 42 is an int, but it will be promoted to double
+
+            Console.ReadKey();
+        }
+    }
+}
