191bf29f8d117dd6a40367b1c6f8b3cba34831fc
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 14:35:38 +0200

    Recursion function example of factorial

--- RecursionFunction/RecursionFunction/Program.cs
+++ RecursionFunction/RecursionFunction/Program.cs
@@ -16,13 +16,29 @@
             // Recursive call
             CountDown(number - 1);
         }
+
+        static int Factorial(int n)
+        {
+            // Base case
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            // Recursive call
+            return n * Factorial(n - 1);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Recursion Function in C#!");
 
-            var age = 30;
+            var age = 11;
 
             CountDown(age);
+
+            for (int i = 1; i <= 5; i++)
+                Console.WriteLine($"{i}! = {Factorial(i)}");
 
             Console.ReadKey(true);
         }
