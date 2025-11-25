a178c233b16635d2853889aa2ed7c3bb3bff809e
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 12:04:46 +0200

    in parameter modifer allows passing a parameter as reference , but don't allow modifying it

--- ParametersModifiers/ParametersModifiers/Program.cs
+++ ParametersModifiers/ParametersModifiers/Program.cs
@@ -2,9 +2,10 @@
 {
     internal class Program
     {
-        static void ModifyValues(ref int num)
+        static void ModifyValues(ref int num, in int age)
         {
-            num += 10;
+            num += age;
+            Console.WriteLine($"Lesego's age is {age} years old");
         }
 
         static void GetValue(out int age, out string name, out string dob)
@@ -19,12 +20,12 @@
             Console.WriteLine("Parameter Modifers in C#!");
 
             int number = 5;
-            int age;
+            int age = 11;
             var name = "";
             var dateOfBirth = "";
 
             Console.WriteLine($"Before ModifyValues: {number}");
-            ModifyValues(ref number);
+            ModifyValues(ref number, age);
             Console.WriteLine($"After ModifyValues: {number}");
 
             GetValue(out age, out name, out dateOfBirth);
