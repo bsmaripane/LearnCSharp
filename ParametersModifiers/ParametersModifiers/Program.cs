c543dcc1e7ca8d0ee39f79da2d5b143fd34cf895
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 11:44:03 +0200

    ref modifer parameter:  - Changes done to the parameter in the method, reflect onthe original parameter

---
+++ ParametersModifiers/ParametersModifiers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ParametersModifiers
+{
+    internal class Program
+    {
+        static void ModifyValues(ref int num)
+        {
+            num += 10;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parameter Modifers in C#!");
+
+            int number = 5;
+
+            Console.WriteLine($"Before ModifyValues: {number}");
+            ModifyValues(ref number);
+            Console.WriteLine($"After ModifyValues: {number}");
+
+            Console.ReadKey();
+        }
+    }
+}
