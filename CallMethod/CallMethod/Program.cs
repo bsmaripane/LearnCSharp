3e820795522733c387192bc7eb1d741244f3c19b
Belmy Maripane <bsmaripane@gmail.com>
2025-11-25 10:02:27 +0200

    A method that returns a value.

--- CallMethod/CallMethod/Program.cs
+++ CallMethod/CallMethod/Program.cs
@@ -11,12 +11,25 @@
         {
             Console.WriteLine($"This argument is passed to the method: {something}");
         }
+
+        static int AddTwoValues(int a, int b)
+        {
+            return a + b;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Call a method!");
             MyMethod();
             WriteSomething("Today is a beautiful day!");
             WriteSomething();
+
+            Console.Write("Enter a number, I'll add 10 to it: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int result = AddTwoValues(num, 10);
+
+            Console.WriteLine($"The result is: {result}");
 
             Console.ReadKey();
         }
