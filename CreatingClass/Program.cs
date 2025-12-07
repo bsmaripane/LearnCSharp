2255ffc6c6893ce89758de51ecb382005c635b00
Belmy Maripane <bsmaripane@gmail.com>
2025-12-07 10:20:54 +0200

    Refactor age calculations and improve output readability
    Replaced hardcoded age calculations with an `ages` array for better maintainability. Updated `Console.WriteLine` statements to use the `ages` array and added descriptive labels to improve output clarity.

--- CreatingClass/Program.cs
+++ CreatingClass/Program.cs
@@ -13,16 +13,18 @@
             int a = 11;
             int b = 3;
 
+            var ages = new int[] { 3, 11, 30, 32,  53 };
+
             Calculator addition = new Calculator();
             int x = addition.add(a, b);
 
             Console.WriteLine(x);
-            Console.WriteLine(addition.add(32, 30));
-            Console.WriteLine(addition.add(56, -53));
-            Console.WriteLine(addition.add(56, -32));
-            Console.WriteLine(addition.add(56, -30));
-            Console.WriteLine(addition.add(53, -32));
-            Console.WriteLine(addition.add(53, -30));
+            Console.WriteLine($"Age: Lesego + Kganya = {addition.add(ages[1], ages[0])}");
+            Console.WriteLine($"Age: Lesego - Kganya = {addition.add(ages[1], -ages[0])}");
+            Console.WriteLine($"Age: Belmy + Tracy = {addition.add(ages[2], ages[3])}");
+            Console.WriteLine($"Age: Belmy - Tracy = {addition.add(ages[2], -ages[3])}");
+            Console.WriteLine($"Age: Emily - Tracy = {addition.add(ages[4], -ages[3])}");
+            Console.WriteLine($"Age: Emily - Belmy = {addition.add(ages[4], -ages[2])}");
 
             Console.ReadKey();
         }
