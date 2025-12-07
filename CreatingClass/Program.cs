3fddc8f5d69224158868451781f04f33b7a2cd49
Belmy Maripane <bsmaripane@gmail.com>
2025-12-07 09:23:53 +0200

    Add C# project and Logical+Operations PDF
    This commit introduces a new C# project named "CreatingClass" with a solution file, project file, and a program demonstrating the creation and usage of a `Calculator` class. The `Calculator` class includes an `add` method for integer addition, and the program outputs results to the console.
    
    Additionally, a PDF file named "Logical+Operations.pdf" was added, containing binary data related to logical operations. The purpose of this file is not explicitly clear from the provided context.

---
+++ CreatingClass/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CreatingClass
+{
+    public class Calculator
+    {
+        public int add(int x, int y) => x+y;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Creating a class and object!");
+
+            int a = 11;
+            int b = 3;
+
+            Calculator addition = new Calculator();
+            int x = addition.add(a, b);
+
+            Console.WriteLine(x);
+            Console.WriteLine(addition.add(32, 30));
+            Console.WriteLine(addition.add(56, -53));
+            Console.WriteLine(addition.add(56, -32));
+            Console.WriteLine(addition.add(56, -30));
+            Console.WriteLine(addition.add(53, -32));
+            Console.WriteLine(addition.add(53, -30));
+
+            Console.ReadKey();
+        }
+    }
+}
