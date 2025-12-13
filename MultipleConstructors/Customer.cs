d3dd7280bc3b24f055edc5916f29d8a4ebab34d5
Belmy Maripane <bsmaripane@gmail.com>
2025-12-13 16:57:42 +0200

    Add two default constructors that one take single string and other takes three strings parameters

---
+++ MultipleConstructors/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleConstructors
+{
+    internal class Customer
+    {
+        // Class property
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+
+        // Class constructor
+        public Customer(string name, string address, string contactNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+        }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- MultipleConstructors/Program.cs
+++ MultipleConstructors/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Using multiple constructor!\n");
+            Customer belmy = new Customer("Belmy");
+            Customer lessego = new Customer("Lesego", "10352 Vergelegen C, JANE FURSE 1085", "+2769 120 0195");
+
+            Console.WriteLine($"Name of Customer: {belmy.Name}");
+            Console.WriteLine($"Name of Customer: {lessego.Name}");
+            Console.WriteLine($"Address of Customer: {lessego.Address}");
+            Console.WriteLine($"Contact of Customer: {lessego.ContactNumber}");
+
+            Console.ReadKey();
         }
     }
 }
