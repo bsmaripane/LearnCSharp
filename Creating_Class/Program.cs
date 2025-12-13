ea4286973ec02d6fa16505b271b20e78bc88d9c8
Belmy Maripane <bsmaripane@gmail.com>
2025-12-13 18:22:41 +0200

    Moved the project from C++ dir C# dir and add method to the car class

---
+++ Creating_Class/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CreatingClass
+{
+    public class Calculator
+    {
+        public int add(int x, int y) => x+y;
+    }
+
+    public enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Creating a class and object!");
+
+            int a = 11;
+            int b = 3;
+
+            var ages = new int[] { 3, 11, 30, 32,  53 };
+
+            Calculator addition = new Calculator();
+            int x = addition.add(a, b);
+
+            Console.WriteLine(x);
+            Console.WriteLine($"Age: Lesego + Kganya = {addition.add(ages[1], ages[0])}");
+            Console.WriteLine($"Age: Lesego - Kganya = {addition.add(ages[1], -ages[0])}");
+            Console.WriteLine($"Age: Belmy + Tracy = {addition.add(ages[2], ages[3])}");
+            Console.WriteLine($"Age: Belmy - Tracy = {addition.add(ages[2], -ages[3])}");
+            Console.WriteLine($"Age: Emily - Tracy = {addition.add(ages[4], -ages[3])}");
+            Console.WriteLine($"Age: Emily - Belmy = {addition.add(ages[4], -ages[2])}");
+
+            Console.WriteLine(Days.Monday);
+
+            Console.ReadKey();
+        }
+    }
+}
