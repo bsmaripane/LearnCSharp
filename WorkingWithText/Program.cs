15c30b4047827840cfa8a7e515b78feab95eead5
Belmy Maripane <bsmaripane@gmail.com>
2025-12-13 14:10:23 +0200

    Woking with text

---
+++ WorkingWithText/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkingWithFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+
+            Console.ReadKey();
+        }
+    }
+}
