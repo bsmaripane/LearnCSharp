8893a9814c5b00f97d4f7249b389f2008aee08f5
Belmy Maripane <bsmaripane@gmail.com>
2025-12-14 14:00:02 +0200

    Overloaded the + operator to add two vector objects

---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using Operator Overloading!\n]");
+
+            Vector v1 = new Vector(3, 5);
+            Vector v2 = new Vector(7, 2);
+            Vector result = v1 + v2;
+
+            result.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OperatorOverloading/Vector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    internal class Vector
+    {
+        public int X {  get; set; }
+        public int Y {  get; set; }
+        public Vector(int x, int y)
+        {
+            X = x; Y = y;
+        }
+        public void Display() => Console.WriteLine($"Vector: ({X}, {Y})");
+
+        public static Vector operator +(Vector v1, Vector v2) => new Vector(v1.X+v2.X,v1.Y+v2.Y);
+    }
+}
