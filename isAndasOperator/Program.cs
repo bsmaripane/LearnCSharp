7909546d6422891bb248955170c9733d9e017b5f
Belmy Maripane <bsmaripane@gmail.com>
2025-12-14 17:43:02 +0200

    Checking Object Type with is

---
+++ isAndasOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace isAndasOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myPet = new Dog();
+
+            if (myPet is Dog)
+                Console.WriteLine("myPet is a Dog");
+            else
+                Console.WriteLine("myPet is NOT a Dog");
+
+            Console.ReadKey();
+        }
+    }
+}
