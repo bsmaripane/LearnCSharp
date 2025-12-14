5e5a229987f8f6ddc9802bf4456455f8d7a39faf
Belmy Maripane <bsmaripane@gmail.com>
2025-12-14 13:13:27 +0200

    Using named paramaters and default parameters oof the method

---
+++ NamedParameters/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NamedParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Salary R{AddBonus(6784.79)}");
+            Console.WriteLine($"Salary R{AddBonus(11458.88, allowance: 3500)}");
+            Console.WriteLine($"Salary R{AddBonus(21458.88, bonus: 6500)}");
+            Console.WriteLine($"Salary R{AddBonus(salary: 38458.88, bonus: 1500, allowance: 1800)}");
+            Console.WriteLine($"Salary R{AddBonus(bonus: 20000, allowance: 8500, salary: 65458.88)}");
+
+            Console.ReadKey();
+        }
+
+        static double AddBonus(double salary, double bonus = 0.0, double allowance = 0.0) => salary += (bonus + allowance);
+    }
+}
