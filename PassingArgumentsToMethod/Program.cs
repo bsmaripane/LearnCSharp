d5583f0ca5431b50073ac8dbf4df5bd1cb955966
Belmy Maripane <bsmaripane@gmail.com>
2025-12-14 14:43:51 +0200

    Passing argument by  value

---
+++ PassingArgumentsToMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PassingArgumentsToMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Passing Arguments by Value!\n");
+            int number = 11;
+            ModifyValue(number);
+            Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+
+        static void ModifyValue(int num)
+        {
+            num = 100;
+        }
+    }
+}
