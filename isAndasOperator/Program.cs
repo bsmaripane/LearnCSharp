f2f1606448e1462798c225e7b7a1b87c7af44cd1
Belmy Maripane <bsmaripane@gmail.com>
2025-12-14 17:47:22 +0200

    Pattern Matching with is

--- isAndasOperator/Program.cs
+++ isAndasOperator/Program.cs
@@ -7,6 +7,10 @@
             Animal myPet = new Dog();
 
             Console.WriteLine(myPet is Dog ? "myPet is a Dog" : "myPet is NOT a Dog");
+
+            object obj = "Hello C# language";
+            if (obj is string message)
+                Console.WriteLine($"The message is: {message}");
            
             Console.ReadKey();
         }
