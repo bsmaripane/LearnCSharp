c34ee70283931e81ff943d9d7f01c3d011872c6a
Belmy Maripane <bsmaripane@gmail.com>
2025-12-14 17:44:35 +0200

    If statement using a oneline

--- isAndasOperator/Program.cs
+++ isAndasOperator/Program.cs
@@ -6,11 +6,8 @@
         {
             Animal myPet = new Dog();
 
-            if (myPet is Dog)
-                Console.WriteLine("myPet is a Dog");
-            else
-                Console.WriteLine("myPet is NOT a Dog");
-
+            Console.WriteLine(myPet is Dog ? "myPet is a Dog" : "myPet is NOT a Dog");
+           
             Console.ReadKey();
         }
     }
