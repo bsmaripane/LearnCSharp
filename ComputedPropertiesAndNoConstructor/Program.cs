6e24dcfeb1a223835948414feff14a7c4cf90c54
Belmy Maripane <bsmaripane@gmail.com>
2025-12-14 16:04:33 +0200

    Computed Properties  as read only

---
+++ ComputedPropertiesAndNoConstructor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ComputedPropertiesAndNoConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Computed Properties and No Constructor!\n");
+            Rectange rec1 = new Rectange();
+            rec1.Width = 3;
+            rec1.Height = 11;
+            Console.WriteLine($"Area of rec1 is {rec1.Area}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ComputedPropertiesAndNoConstructor/Rectange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputedPropertiesAndNoConstructor
+{
+    internal class Rectange
+    {
+        public double Width {  get; set; }
+        public double Height {  get; set; }
+
+        // Computed property - read only property
+        public double Area
+        {
+            get { return Width * Height; }
+        }
+    }
+}
