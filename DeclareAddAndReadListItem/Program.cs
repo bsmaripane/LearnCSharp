92608512d2c1f7a103ece6265b9ab878e820c212
Belmy Maripane <bsmaripane@gmail.com>
2025-12-16 20:39:03 +0200

    Declare a List, add and read List item

---
+++ DeclareAddAndReadListItem/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DeclareAddAndReadListItem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Declare a List, add and read List item!");
+            // Declaring a list and initialising
+            List<string > colors = new List<string>();
+
+            // Adding items to the list
+            colors.Add("green");
+            colors.Add("blue");
+            colors.Add("red");
+            colors.Add("orange");
+            colors.Add("black");
+
+            foreach (string color in colors) 
+                Console.WriteLine($"Current color: {color}");
+
+            Console.ReadKey();
+        }
+    }
+}
