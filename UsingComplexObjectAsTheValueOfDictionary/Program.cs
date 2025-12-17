1d951f5204cf3131deb6b8eaa3b3b9456c23aa79
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 14:14:19 +0200

    Declaring a dictionary o fan object and adding data to the dictionary

---
+++ UsingComplexObjectAsTheValueOfDictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace UsingComplexObjectAsTheValueOfDictionary
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, int age, double salary)
+        {
+            Name = name;
+            Age = age;
+            Salary = salary;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring dictionary
+            Dictionary<int,Employee> employees = new Dictionary<int,Employee>();
+
+            // Add new employee
+            employees.Add(1, new Employee("John Doe", 36, 13435.70));
+            employees.Add(2, new Employee("James Stork", 26, 15767.70));
+            employees.Add(3, new Employee("Joseph Ask", 27, 10435.70));
+            employees.Add(4, new Employee("Alex Alex", 30, 23435.70));
+            employees.Add(5, new Employee("Major Major", 46, 33435.70));
+
+            //
+
+            Console.ReadKey();
+        }
+    }
+}
