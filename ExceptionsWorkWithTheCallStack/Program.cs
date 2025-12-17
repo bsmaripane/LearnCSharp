fa845595449850b93dd9b5d1bfdb423cd8be27fe
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 23:30:55 +0200

    How exceptions work with the call stack

---
+++ ExceptionsWorkWithTheCallStack/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ExceptionsWorkWithTheCallStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LevelOne();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught in Main: {ex.Message}");
+            }
+            
+            Console.ReadKey();
+        }
+
+        static void LevelOne()
+        {
+            LevelTwo();
+        }
+
+        static void LevelTwo()
+        {
+            throw new Exception("Something went wrong!");
+        }
+    }
+}
