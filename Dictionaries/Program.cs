14f34c5b9515a879fbaa46a92e946a2a626a41d7
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 13:25:08 +0200

    Declaring an initialising a dictionary

---
+++ Dictionaries/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Key - value
+            // Declaring an initialising a dictionary
+            Dictionary<int, string> employee = new Dictionary<int, string>();
+            employee.Add(101, "John Doe");
+            employee.Add(102, "Jane Doe");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
