d95f3f738462c03fa2341b34d4fb756821ba6fc2
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 21:46:43 +0200

    Attempting to divide with zero and handle the exception using Try/Catch

---
+++ TryCatchExceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TryCatchExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            Console.WriteLine("Please enter a number");
+            //int num1 = int.Parse(Console.ReadLine());
+            int num1 = 0;
+            int num2 = 2;
+
+            try
+            {
+                result = num2 / num1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
+
+            Console.WriteLine($"Result: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
