9c1d1f654cc431a0865327c6881a55497e4c8b8b
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 16:18:43 +0200

    Declarin and initialising a dictionary of string key

---
+++ DicktionaryWithStringKey/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DicktionaryWithStringKey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarin and initialising a dictionary of string key
+            var codes = new Dictionary<string, string>
+            {
+                ["NY"] = "New York",
+                ["CA"] = "California",
+                ["TX"] = "Texas"
+            };
+
+
+            Console.ReadKey();
+        }
+    }
+}
