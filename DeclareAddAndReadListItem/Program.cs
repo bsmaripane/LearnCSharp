23c9d129c7a7df81c955ca9d9dd8521dfe802b23
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 07:56:02 +0200

    FindAll numbers greater than or equal to ten

--- DeclareAddAndReadListItem/Program.cs
+++ DeclareAddAndReadListItem/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DeclareAddAndReadListItem
+﻿using System;
+
+namespace DeclareAddAndReadListItem
 {
     internal class Program
     {
@@ -52,6 +54,13 @@
 
             Console.Write("\n\nSorded list: ");
             foreach (int number in numbers)
+                Console.Write($"{number} ");
+
+            // Find item in a List - return  a list of numbers that are 10 and higher
+            List<int> greaterOrEqualToTen = numbers.FindAll(x => x >= 10);
+
+            Console.Write("\n\nlist number greater or equal to ten: ");
+            foreach (int number in greaterOrEqualToTen)
                 Console.Write($"{number} ");
 
             Console.ReadKey();
