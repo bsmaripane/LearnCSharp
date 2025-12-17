11b32e172fbcd36e6444f1901e56d45f288eb844
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 11:22:22 +0200

    A int? is a nullable int

---
+++ NullablesInCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NullablesInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? age = null; // int? is a nullable int
+
+            Console.ReadKey();
+        }
+    }
+}
