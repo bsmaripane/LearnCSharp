be4afc0fec416129d0cabeaae9b26dbfb81e45dc
Belmy Maripane <bsmaripane@gmail.com>
2025-12-17 13:34:25 +0200

    Update data in a dictionary

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -10,8 +10,13 @@
             employee.Add(101, "John Doe");
             employee.Add(102, "Jane Doe");
 
+            // accessing data in dictionary
             string name = employee[101];
             Console.WriteLine(name);
+
+            // Update data in a dictionary
+            employee[102] = "Jane Smith";
+            Console.WriteLine(employee[102]);
 
             Console.ReadKey();
         }
