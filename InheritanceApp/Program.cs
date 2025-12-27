d778e5c5a02a094447cbd2cf4f33a8dd8b82bce0
Belmy Maripane <bsmaripane@gmail.com>
2025-12-27 10:05:47 +0200

    Add the basic of the inheritance.

---
+++ InheritanceApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace InheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance App!");
+            Dog dog = new Dog();
+
+            dog.Bark();
+            dog.Eat();
+            dog.Bark();
+
+            Console.ReadKey();
+        }
+    }
+
+    // Base class
+    class Animal
+    {
+        public void Eat() { Console.WriteLine("Eatng..."); }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        public void Bark() { Console.WriteLine("Barking..."); }
+    }
+}
