678164f2726547497a90305423e6973208b0d258
Belmy Maripane <bsmaripane@gmail.com>
2025-12-27 10:32:44 +0200

    The analogy for IS-A relationship

---
+++ Section_8_IS-A_Relationship/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Section_8_IS_A_Relationship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HAS-A Relationship\n");
+
+            Car myCar = new Car();
+            myCar.Move();   // Inherited from Vehicle
+            myCar.Honk();   // Specific to Car
+
+            Console.ReadKey();
+        }
+    }
+
+    class vehicle
+    {
+        public void Move() { Console.WriteLine("The vehicle is moving"); }
+    }
+
+    class Car : vehicle
+    {
+        public void Honk() { Console.WriteLine("The car is honking"); }
+    }
+}
