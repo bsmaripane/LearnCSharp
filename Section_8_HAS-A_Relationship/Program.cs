39ac1ebb1bf3658834d29e89781623ba2f021afb
Belmy Maripane <bsmaripane@gmail.com>
2025-12-27 10:45:15 +0200

    Add the analogy of the HAS-A relationship - composition

---
+++ Section_8_HAS-A_Relationship/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Section_8_HAS_A_Relationship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HAS-A Relationship - Composition\n");
+
+            Car myCar = new Car();
+            myCar.StartCar();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Engine
+    {
+        public void Start() { Console.WriteLine("Engine started"); }
+    }
+
+    class Car
+    {
+        private Engine engine = new Engine();
+        public void StartCar()
+        {
+            engine.Start();     // Car contains an Engine and uses it
+            Console.WriteLine("Car is ready to drive");
+        } 
+    }
+}
