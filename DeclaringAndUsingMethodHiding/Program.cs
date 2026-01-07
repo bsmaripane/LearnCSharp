a482f45a9febec154cf72e775e467cfb64e42210
Belmy Maripane <bsmaripane@gmail.com>
2026-01-07 09:30:15 +0200

    Add a derived class that hides the base class method

---
+++ DeclaringAndUsingMethodHiding/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DeclaringAndUsingMethodHiding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Declaring anf Using Method Hiding\n!");
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Animal
+    {
+        public void Speak() { Console.WriteLine("The animal makes a sound."); }
+    }
+
+    class Dog : Animal
+    {
+        public new void Speak() { Console.WriteLine("The dog is barks."); } 
+    }
+}
