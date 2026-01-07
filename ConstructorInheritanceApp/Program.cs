5667a20841918190296a97ea130c6cca95a8512b
Belmy Maripane <bsmaripane@gmail.com>
2026-01-07 21:17:36 +0200

    Constructor inheritance

---
+++ ConstructorInheritanceApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConstructorInheritanceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee maxine = new Employee("Maxine", 29);
+            maxine.DisplayPersonInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+
+        // Base class constructor
+        public Person(string name, int age) 
+        {
+            Name = name;
+            Age = age;
+            Console.WriteLine("Person constructor called"); 
+        }
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public Employee(string name, int age) : base(name, age) { Console.WriteLine("Employee (Derived Class) constructor called"); }
+    }
+}
