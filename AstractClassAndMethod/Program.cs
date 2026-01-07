4bdd0198455d5c8f35fdcbf0b7729a2b0d174d18
Belmy Maripane <bsmaripane@gmail.com>
2026-01-07 19:34:43 +0200

    Add the abstract class - cannot be instantiated

---
+++ AstractClassAndMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AstractClassAndMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Defining an Astract Class and Method!\n");
+
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract void MakeSound();
+        public void Sleep() { Console.WriteLine("Sleeping..."); }
+    }
+
+
+}
