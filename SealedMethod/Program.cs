637a2444e94022aa5125f4aba68229f5951a7174
Belmy Maripane <bsmaripane@gmail.com>
2026-01-07 10:07:21 +0200

    Create a base class with a virtual method

---
+++ SealedMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SealedMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo of the Sealad Method in C#!\n");
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Animal
+    {
+        public virtual void MakeSound() { Console.WriteLine("The animal makes a sound"); }
+    }
+
+
+}
