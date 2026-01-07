9bf600e04218f627a8a50ab7771314039030cb98
Belmy Maripane <bsmaripane@gmail.com>
2026-01-07 08:49:04 +0200

    Add AccessModifiers project demonstrating C# access levels
    Created new .NET 9.0 project "AccessModifiers" with example code in Program.cs illustrating public, protected, and private access modifiers. Includes BaseClass and DerivedClass to show field accessibility and usage. Added solution and project files.

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Access Modifiers!\n");
+            BaseClass baseCalss = new BaseClass();
+            baseCalss.ShowFields();
+
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.AccessFields();
+            derivedClass.ShowFields();
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class BaseClass
+    {
+        // access modifiers
+        public int publicField;
+        protected int protectedField;
+        private int privateField;
+
+        public void ShowFields()
+        {
+            Console.WriteLine($"Public: {publicField}, Protected: {protectedField} and Private: {privateField}");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public void AccessFields()
+        {
+            publicField = 1;
+            protectedField = 2;
+        }
+    }
+}
