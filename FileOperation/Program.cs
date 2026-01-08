9736836d48040422d895d60e7619d2d7791501bb
Belmy Maripane <bsmaripane@gmail.com>
2026-01-08 11:22:04 +0200

    Add writing to a file and reading from a file

---
+++ FileOperation/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FileOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter("file.txt"))
+            {
+                sw.WriteLine("My name is Belmy Semape Maripane,\nI love Felicia van der Merwe, actually I'm in love with her, she is my everything,\nand I truly want to marry her before end of 2026.");
+            }
+
+            using (StreamReader sr = new StreamReader("file.txt"))
+            {
+                string content = sr.ReadToEnd();
+                Console.WriteLine(content);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
