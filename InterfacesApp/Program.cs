92c383a7c486ace5e83ef639bf1d89d93321f5eb
Belmy Maripane <bsmaripane@gmail.com>
2026-01-08 09:43:10 +0200

    Add the syntax of the interface

---
+++ InterfacesApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace InterfacesApp
+{
+    public interface IAnimal
+    {
+        void MakeSound();
+        string Eat(string food);
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
