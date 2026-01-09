f54da3dc7276afbd98b1105dc6625ac514161fbd
Belmy Maripane <bsmaripane@gmail.com>
2026-01-09 17:11:11 +0200

    Add a line that create a file at the specific directory and append a hello world

---
+++ CreateFileApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CreateFileApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.AppendAllText("C:\\Users\\bsmar\\OneDrive\\Desktop\\Udemy\\LearnCSharp\\CreateFileApp\\Log.txt", "Hello World\n");
+
+            Console.ReadKey();
+        }
+    }
+}
