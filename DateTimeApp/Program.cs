1a255771137c49827e292e531cb5ea9a19500666
Belmy Maripane <bsmaripane@gmail.com>
2026-01-12 20:47:47 +0200

    Create an object of dateTime and specify a date

---
+++ DateTimeApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DateTimeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime birthDate = new DateTime(1995, 5, 17);
+
+            Console.WriteLine($"My birthday is {birthDate}");
+
+            Console.ReadKey();
+        }
+    }
+}
