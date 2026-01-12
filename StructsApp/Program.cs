2a6828d38cc773445fba75edc4189c06ba4f1b4a
Belmy Maripane <bsmaripane@gmail.com>
2026-01-12 16:31:29 +0200

    Simple structs in C#

---
+++ StructsApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace StructsApp
+{
+    public struct Point
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y) { X = x; Y = y; }
+
+        public void Display() { Console.WriteLine($"Point: ({X}, {Y})"); }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point(29,30);
+            p.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
