e361ab10cc16e4d631277b8a5f6174955f64ceb8
Belmy Maripane <bsmaripane@gmail.com>
2026-01-12 20:31:32 +0200

    The use of enums in C#

---
+++ EnumsInC#/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EnumsInC_
+{
+    enum Day { Mon, Tue, Wed, Thu, Fri, Sat, Sun };
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day mo = Day.Mon;
+            Day tu = Day.Tue;
+            Day we = Day.Wed;
+            Day thu = Day.Thu;
+            Day fr = Day.Fri;
+
+            Console.WriteLine(fr == mo);
+            Console.WriteLine(Day.Sun);
+            Console.WriteLine(Day.Sat);
+            Console.WriteLine((int)Day.Wed);
+
+            Console.ReadKey();
+        }
+    }
+}
