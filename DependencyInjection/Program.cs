fede12f1cf12ac0760c0c24a5208cf66530df4d9
Belmy Maripane <bsmaripane@gmail.com>
2026-01-12 08:36:52 +0200

    Tightly coupled classes without dependency injection

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DependencyInjection
+{
+    public class EmailService
+    {
+        public void SendEmail(string message) { Console.WriteLine($"Sending email: {message}"); }
+    }
+
+    public class UserService
+    {
+        private EmailService _emailService;
+        public UserService() { _emailService = new EmailService(); }   // Directly creating the dependancy
+
+        public void RegisterUser(string name)
+        {
+            Console.WriteLine($"User {name} registeed successfully.");
+            _emailService.SendEmail($"Welcome {name}!");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserService userService = new UserService();
+            userService.RegisterUser("John Doe");
+
+
+            Console.ReadKey();
+        }
+    }
+}
