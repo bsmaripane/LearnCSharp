dba9b2b2c0a435c2f872c99bae35c53b19a99961
Belmy Maripane <bsmaripane@gmail.com>
2026-01-12 16:33:38 +0200

    Using fields instead of the properties

--- StructsApp/Program.cs
+++ StructsApp/Program.cs
@@ -2,8 +2,10 @@
 {
     public struct Point
     {
-        public int X {  get; set; }
-        public int Y { get; set; }
+        //public int X {  get; set; }
+        //public int Y { get; set; }
+        public int X;
+        public int Y;
 
         public Point(int x, int y) { X = x; Y = y; }
 
@@ -15,6 +17,11 @@
         {
             Point p = new Point(29,30);
             p.Display();
+
+            Point p1;
+            p1.X = 3;
+            p1.Y = 11;
+            p1.Display();
 
             Console.ReadKey();
         }
