078bf7813bf149641155b107a101c1d4e2d8f70e
Belmy Maripane <bsmaripane@gmail.com>
2026-01-12 14:53:33 +0200

    Achiving multiple inheritance using the interface

---
+++ MultipleInheritanceApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MultipleInheritanceApp
+{
+    public interface IPrintable
+    {
+        void Print();
+    }
+
+    public interface IScannable
+    {
+        void Scan();
+    }
+
+    public class MultiFunctionPrinter : IPrintable, IScannable
+    {
+        public void Print() { Console.WriteLine("Printing documents"); }
+        public void Scan() { Console.WriteLine("Scanning documents"); }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiFunctionPrinter printer = new MultiFunctionPrinter();
+            printer.Print();
+            printer.Scan();
+
+            Console.ReadKey();
+        }
+    }
+}
