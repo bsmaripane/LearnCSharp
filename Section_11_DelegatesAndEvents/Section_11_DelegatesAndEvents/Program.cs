1a2885d7b16e7d9fc1b371fe3ee639a30e82ea5b
Belmy Maripane <bsmaripane@gmail.com>
2026-01-28 08:55:11 +0200

    Add an introduction example of a delegate

---
+++ Section_11_DelegatesAndEvents/Section_11_DelegatesAndEvents/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Section_11_DelegatesAndEvents
+{
+    internal class Program
+    {
+        // 1. Declaration:
+        public delegate void Notify(string message);
+         
+        static void Main(string[] args)
+        {
+            // Delegates define a method signature, and any method assigned to a delegate must match this signature
+
+            // 2. Instatiation:
+            Notify notifyDelegate = ShowMessage;
+            //Notify notifyDelegate = new Notify(notifyDelegate); // old style
+
+            // 3. Invocation:
+            notifyDelegate("Morning Delegate");
+
+            Console.ReadKey();
+        }
+
+        // Mehod
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
