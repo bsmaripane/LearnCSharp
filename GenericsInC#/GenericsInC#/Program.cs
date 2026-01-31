149d995ad7869b1293bceec9b5c1b4d06442bf50
Belmy Maripane <bsmaripane@gmail.com>
2026-01-31 07:04:04 +0200

    Add another method to print string elements of an array

--- GenericsInC#/GenericsInC#/Program.cs
+++ GenericsInC#/GenericsInC#/Program.cs
@@ -7,11 +7,21 @@
             foreach (var number in numbersArray)
                 Console.WriteLine(number);
         }
+        static void PrintArray(string[] wordsArray)
+        {
+            foreach (var word in wordsArray)
+                Console.WriteLine(word);
+        }
+
         static void Main(string[] args)
         {
             int[] numbersArray = { 1, 2, 3, 4, 5 };
 
             PrintArray(numbersArray);
+
+            string[] names = { "Belmy", "Felicia", "Maxine", "Thapelo" };
+
+            PrintArray(names);
 
             Console.ReadKey();
         }
