ee6eccd60a09014e982e044a4a0f2f0e287e88e9
Belmy Maripane <bsmaripane@gmail.com>
2026-01-31 07:01:06 +0200

    Add a method to print the elements of an array

---
+++ GenericsInC#/GenericsInC#/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericsInC_
+{
+    internal class Program
+    {
+        static void PrintArray(int[] numbersArray)
+        {
+            foreach (var number in numbersArray)
+                Console.WriteLine(number);
+        }
+        static void Main(string[] args)
+        {
+            int[] numbersArray = { 1, 2, 3, 4, 5 };
+
+            PrintArray(numbersArray);
+
+            Console.ReadKey();
+        }
+    }
+}
