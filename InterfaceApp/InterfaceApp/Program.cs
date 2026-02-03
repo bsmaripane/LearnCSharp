8bfbff251432235158c72d506cf6ce66589c1970
Belmy Maripane <bsmaripane@gmail.com>
2026-02-03 22:24:12 +0200

    Add InterfaceApp project with IEnemy interface example
    Introduced a new C# console app "InterfaceApp" targeting .NET 9.0.
    Added IEnemy interface and two implementations: Dragon and Vampire.
    Demonstrated polymorphism by invoking Attack and Defend methods
    on a list of IEnemy objects in the Main method.

---
+++ InterfaceApp/InterfaceApp/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace InterfaceApp
+{
+    public interface IEnemy
+    {
+        void Attack();
+        void Defend();
+    }
+
+    public class Dragon : IEnemy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Dragon breathes fire~");
+        }
+
+        public void Defend()
+        {
+            Console.WriteLine("Dragon spreads its wings to shield itself.");
+        }
+    }
+
+    public class Vampire : IEnemy
+    {
+        public void Attack()
+        {
+            Console.WriteLine("Vampire strikes with its fangs.");
+        }
+
+        public void Defend()
+        {
+            Console.WriteLine("Vampire transforms into a bat to evade.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnemy enemy1 = new Dragon();
+            IEnemy enemy2 = new Vampire();
+
+            List<IEnemy> enemyList = new List<IEnemy>();
+            enemyList.Add(enemy1);
+            enemyList.Add(enemy2);
+
+            foreach (IEnemy enemy in enemyList)
+            {
+                enemy.Attack();
+                enemy.Defend();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
