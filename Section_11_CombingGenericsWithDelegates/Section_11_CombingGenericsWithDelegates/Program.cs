0dcc661955f30983021bb985b842ca4dc6fcc6bb
Belmy Maripane <bsmaripane@gmail.com>
2026-02-05 22:25:54 +0200

    Add the combining of generic with delegates

--- Section_11_CombingGenericsWithDelegates/Section_11_CombingGenericsWithDelegates/Program.cs
+++ Section_11_CombingGenericsWithDelegates/Section_11_CombingGenericsWithDelegates/Program.cs
@@ -5,7 +5,7 @@
     public class Person
     {
         public int Age { get; set; }
-        public string name { get; set; }
+        public string Name { get; set; }
     }
 
     public class PersonSort
@@ -19,6 +19,7 @@
                     // Compare people[i] and pwoplw[j] using the provided comparison delegate
                     if (comparison(people[i], people[j]) > 0)
                     {
+                        // swap people[i[ and people[j] if they are in the wrong order
                         Person temp = people[i];
                         people[i] = people[j];
                         people[j] = temp;
@@ -32,9 +33,32 @@
     {
         static void Main(string[] args)
         {
-            
+            Person[] people = {
+                new Person { Name = "Belicia", Age = 4 },
+                new Person { Name = "Belmy", Age = 30},
+                new Person { Name = "Felicia", Age = 28}
+            };
+
+            PersonSort sorter = new PersonSort();
+            sorter.Sort(people, CompareByAge);
+
+            foreach(Person person in people)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age}");
+            }
 
             Console.ReadKey();
+        }
+
+        static int CompareByAge(Person x, Person y)
+        {
+            return x.Age.CompareTo(y.Age);
+        }
+
+
+        static int CompareByName(Person x, Person y)
+        {
+            return x.Name.CompareTo(y.Name);
         }
     }
 }
