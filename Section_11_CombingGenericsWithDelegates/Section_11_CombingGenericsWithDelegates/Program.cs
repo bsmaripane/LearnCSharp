b8b7332d7694ad09468735c2c6be6154647b5417
Belmy Maripane <bsmaripane@gmail.com>
2026-02-05 20:04:43 +0200

    Compare people[i] and pwoplw[j] using the provided comparison delegate

---
+++ Section_11_CombingGenericsWithDelegates/Section_11_CombingGenericsWithDelegates/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Section_11_CombingGenericsWithDelegates
+{
+    public delegate int Comparison<T>(T x, T y);
+
+    public class Person
+    {
+        public int Age { get; set; }
+        public string name { get; set; }
+    }
+
+    public class PersonSort
+    {
+        public void Sort(Person[] people, Comparison<Person> comparison)
+        {
+            for(int i = 0; i < people.Length - 1; i++)
+            {
+                for(int j = i + 1; j < people.Length; j++)
+                {
+                    // Compare people[i] and pwoplw[j] using the provided comparison delegate
+                    if (comparison(people[i], people[j]) > 0)
+                    {
+                        Person temp = people[i];
+                        people[i] = people[j];
+                        people[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+    }
+}
