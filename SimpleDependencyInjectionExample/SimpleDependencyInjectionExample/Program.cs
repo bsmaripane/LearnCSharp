c4c5dcb7ce49e7a79e8148029d89c6cf0e213f2a
Belmy Maripane <bsmaripane@gmail.com>
2026-02-06 09:48:05 +0200

    A tightly coupled of dependency classes
    - Student forces the use of pen
    - If Pen changes -> Student breaks

---
+++ SimpleDependencyInjectionExample/SimpleDependencyInjectionExample/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleDependencyInjectionExample
+{
+    public class Pen
+    {
+        public void Write()
+        {
+            Console.WriteLine("Writing with a pen");
+        }
+    }
+
+    class Student
+    {
+        Pen pen = new Pen();
+
+        public void DoHomework(string active)
+        {
+            Console.Write($"{active}: ");
+            pen.Write();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            student.DoHomework("Draw a mind map");
+            student.DoHomework("Write an essay of about yourself with 1000 words");
+            
+            Console.ReadKey();
+        }
+    }
+}
