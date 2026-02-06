f9884e2ebcfd79a6ed586f3a3b68835a439cdc45
Belmy Maripane <bsmaripane@gmail.com>
2026-02-06 10:00:32 +0200

    Loosely coupled - dependency injection
    - giving, not creating
    - Instead of a class creating what it needs, it is given what it needs

--- SimpleDependencyInjectionExample/SimpleDependencyInjectionExample/Program.cs
+++ SimpleDependencyInjectionExample/SimpleDependencyInjectionExample/Program.cs
@@ -1,6 +1,11 @@
 ﻿namespace SimpleDependencyInjectionExample
 {
-    public class Pen
+    public interface IWritingTool
+    {
+        void Write();
+    }
+
+    public class Pen : IWritingTool
     {
         public void Write()
         {
@@ -8,14 +13,34 @@
         }
     }
 
+    public class Pencil : IWritingTool
+    {
+        public void Write()
+        {
+            Console.WriteLine("Writing with a pencil");
+        }
+    }
+
+    public class Marker : IWritingTool
+    {
+        public void Write()
+        {
+            Console.WriteLine("Writing with a marker");
+        }
+    }
+
     class Student
     {
-        Pen pen = new Pen();
+        IWritingTool _tool;
 
+        public Student (IWritingTool tool)
+        {
+            _tool = tool;
+        }
         public void DoHomework(string active)
         {
             Console.Write($"{active}: ");
-            pen.Write();
+            _tool.Write();
         }
     }
 
@@ -23,10 +48,13 @@
     {
         static void Main(string[] args)
         {
-            Student student = new Student();
+            Student student = new Student(new Pen());
+            Student student1 = new Student(new Pencil());
+            Student student2 = new Student(new Marker());
 
-            student.DoHomework("Draw a mind map");
+            student1.DoHomework("Draw a mind map");
             student.DoHomework("Write an essay of about yourself with 1000 words");
+            student2.DoHomework("Write notes on the board");
             
             Console.ReadKey();
         }
