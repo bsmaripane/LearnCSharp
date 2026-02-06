780000a0279ead0f6892c7e4b52b02f275e9c181
Belmy Maripane <bsmaripane@gmail.com>
2026-02-06 14:22:25 +0200

    Create an event app
    - The 'On' prefix makes it immediately clear that the method is associated with an event
    - Create a subscriber

---
+++ Section_11_EventsApp/Section_11_EventsApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Section_11_EventsApp
+{
+    public delegate void Notify(string message);
+
+    public class EventPublisher
+    {
+        // It signifies that the method is not just a regular method but one that is called when a specific event occurs.
+        public event Notify OnNotify;
+
+        public void RaisEvent(string message)
+        {
+            OnNotify?.Invoke(message);  // Invoke the event if there are any subscribers
+        }
+    }
+
+    public class EvenSubcriber
+    {
+        public void OnEvenRaised(string message)
+        {
+            Console.WriteLine($"Event received: {message}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
