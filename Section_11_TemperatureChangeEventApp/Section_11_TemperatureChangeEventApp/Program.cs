6293461a53b0f2e800ab983112097c22a0c7d19a
Belmy Maripane <bsmaripane@gmail.com>
2026-02-06 14:46:20 +0200

    Create a temperature change event

---
+++ Section_11_TemperatureChangeEventApp/Section_11_TemperatureChangeEventApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Section_11_TemperatureChangeEventApp
+{
+    public delegate void TemperatureChangeHandler(string message);
+
+    public class TemperatureMonitor
+    {
+        public event TemperatureChangeHandler OnTemperatureChanged;
+
+        private int _temperature;
+
+        public int Temperature { get { return _temperature; } 
+            set
+            { 
+                _temperature = value;
+
+                if (_temperature > 30)
+                {
+                    // Raise EVENT!!!
+                    RaiseTemperatureChangedEvent("Temperature is above threshold!");
+                }
+            }
+        }
+
+        protected virtual void RaiseTemperatureChangedEvent(string message)
+        {
+            OnTemperatureChanged?.Invoke(message);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
