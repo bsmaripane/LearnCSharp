7c1a002d138a79ed793c2d773adda82753bc2210
Belmy Maripane <bsmaripane@gmail.com>
2026-02-14 11:41:24 +0200

    Simple example of reading and inserting a data into a database

---
+++ WorksWithDatabaseApp/WorksWithDatabaseApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WorksWithDatabaseApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Database connection 
+            string connectionString = "";
+
+            // Open database communication
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            // SQL command
+            SqlCommand command = new SqlCommand("SELECT * FROM students", connection);
+
+            // Reading data
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Name"]);
+            }
+
+            // Insert data into database
+            SqlCommand insertCommand = new SqlCommand("INSERT INTO students(first_name) VALUES ('Belicia')", connection);
+            insertCommand.ExecuteNonQuery();
+
+            // Closing database connection
+            connection.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
