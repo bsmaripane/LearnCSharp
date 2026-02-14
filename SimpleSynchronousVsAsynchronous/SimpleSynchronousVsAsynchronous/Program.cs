94d11ba9f67012fde1a7f33997420276c6650062
Belmy Maripane <bsmaripane@gmail.com>
2026-02-14 13:34:23 +0200

    Add a simple example of synchronous method

---
+++ SimpleSynchronousVsAsynchronous/SimpleSynchronousVsAsynchronous/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleSynchronousVsAsynchronous
+{
+    internal class Program
+    {
+        public static void GetData()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("Data Loaded");
+        }
+        static void Main(string[] args)
+        {
+            GetData();
+            Console.WriteLine("Synchronous Programming");
+
+            Console.ReadKey();
+        }
+    }
+}
