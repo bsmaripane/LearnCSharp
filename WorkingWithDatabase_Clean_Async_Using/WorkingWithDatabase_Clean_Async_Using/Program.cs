aafcbe392d7685a99efaa671fcd66a763a4f6267
Belmy Maripane <bsmaripane@gmail.com>
2026-02-14 20:54:13 +0200

    Add a database example with clean + async + using
    - Connection opens asynchronously
    - Query runs asynchronously
    - Reading rows asynchronously
    - Using block cleans everything

---
+++ WorkingWithDatabase_Clean_Async_Using/WorkingWithDatabase_Clean_Async_Using/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WorkingWithDatabase_Clean_Async_Using
+{
+    internal class Program
+    {
+        public static async Task GetCustomersAsync()
+        {
+            string connectionString = "Server=MARIPANEBS\\SQLEXPRESS;Database=shop;Integrated Security=True;TrustServerCertificate=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM customers", connection))
+                {
+                    Console.WriteLine("Customer ID\tFirst Name\tLast Name\tEmail Address");
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                            Console.WriteLine($"{reader["customer_id"]}\t\t{reader["first_name"]}\t\t{reader["last_name"]}\t\t{reader["email"]}");
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            _ = GetCustomersAsync();
+
+            Console.ReadKey();
+        }
+    }
+}
