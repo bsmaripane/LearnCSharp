e926800ddf97382b59e42c3e842d15849f1f1d2c
Belmy Maripane <bsmaripane@gmail.com>
2026-02-15 18:47:34 +0200

    Add a test with minimum code and verified behavior

--- StudentApp/StudentApp.Tests/StudentRepositoryTests.cs
+++ StudentApp/StudentApp.Tests/StudentRepositoryTests.cs
@@ -1,11 +1,33 @@
-﻿namespace StudentApp.Tests
+﻿using StudentApp.Data;
+using StudentApp.Models;
+using Xunit;
+
+namespace StudentApp.Tests
 {
     public class StudentRepositoryTests
     {
         [Fact]
-        public void Test1()
+        public async Task AddStudentAsync_Should_Insert_Student_Into_Database()
         {
+            // Arrange
+            string connectionString = "Server=MARIPANEBS\\SQLEXPRESS;Database=StudentDB;Integrated Security=True;TrustServerCertificate=True;";
 
+            var repository = new StudentRepository(connectionString);
+
+            var student = new Student
+            {
+                FirstName = "Felicia",
+                LastName = "Maripane",
+                CourseCode = "EDU101",
+                RegistrationDate = DateTime.Today,
+                CourseFee = 67340
+            };
+
+            // Act
+            await repository.AddStudentAsync(student);
+
+            // Assert
+            Assert.True(true);
         }
     }
 }
