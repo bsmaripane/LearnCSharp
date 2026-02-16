aa40c560aa7a0764cfeedec55476dabb92f00fc8
Belmy Maripane <bsmaripane@gmail.com>
2026-02-16 11:03:46 +0200

    Add a method that extract email addresses from a string and print them

--- Section_12_RegularExpression/Section_12_RegularExpression/Program.cs
+++ Section_12_RegularExpression/Section_12_RegularExpression/Program.cs
@@ -4,6 +4,19 @@ namespace Section_12_RegularExpression
 {
     internal class Program
     {
+        public static void ExtractEmailAddressPattern(string input)
+        {
+            string emailPattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}";
+            Regex regex = new Regex(emailPattern);
+
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                GroupCollection group = match.Groups;
+                Console.WriteLine(group[0].Value);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Regular Expression Example");
@@ -22,6 +35,12 @@ namespace Section_12_RegularExpression
                 GroupCollection group = hit.Groups;
                 Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
             }
+
+            string message = "Hello! You can reach our support team at help@example.com or " +
+                "contact our sales representative at sales_department123@business.org. " +
+                "For general inquiries, email info.test@service-provider.net.";
+
+            ExtractEmailAddressPattern(message);
 
             Console.ReadKey();
         }
