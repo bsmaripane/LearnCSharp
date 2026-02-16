56d57a5fa2f2c1cec5d6c8a746a2383ef43f9048
Belmy Maripane <bsmaripane@gmail.com>
2026-02-16 15:29:10 +0200

    Create a fake repository for understanding purpose
    - No SQL
    - No connection string
    - Everything happens in memory

---
+++ StudentApp/StudentApp.Tests/Fakes/FakeStudentRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentApp.Data;
+using StudentApp.Models;
+
+namespace StudentApp.Tests.Fakes
+{
+    public class FakeStudentRepository : IStudentRepository
+    {
+        private readonly List<Student> _students = new();
+
+        public Task AddStudentAsync(Student student)
+        {
+            _students.Add(student);
+
+            return Task.CompletedTask;
+        }
+
+        public Task<Student> GetStudentByNameAsync(string firstName, string lastName)
+        {
+            var student = _students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+
+            return Task.FromResult(student);
+        }
+
+        public Task<List<Student>> GetAllStudentsAsync()
+        {
+            return Task.FromResult(_students);
+        }
+
+        public Task DeleteStudentByNameAsync(string firstName, string lastName)
+        {
+            var student = _students.FirstOrDefault(s => s.FirstName == firstName && s.LastName == lastName);
+
+            if (student != null)
+                _students.Remove(student);
+
+            return Task.CompletedTask;
+        }
+    }
+}
