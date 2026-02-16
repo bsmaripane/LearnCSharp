e08e4665e678d3267cc23b91b24897124d278a7a
Belmy Maripane <bsmaripane@gmail.com>
2026-02-16 18:01:08 +0200

    Add the order model for the database

---
+++ StudentApp/StudentApp.Tests/StudentServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentApp.Models;
+using StudentApp.Tests.Fakes;
+using Xunit;
+
+
+public class StudentServiceTests
+{
+    [Fact]
+    public async Task FakeRepository_Should_Save_Student()
+    {
+        // Arrange
+        var repository = new FakeStudentRepository();
+
+        var student = new Student
+        { 
+            FirstName = "Mock",
+            LastName = "Interface",
+            CourseCode = "FAKE01",
+            RegistrationDate = DateTime.Today,
+            CourseFee = 0
+        };
+
+        // Act
+        await repository.AddStudentAsync(student);
+        var result = await repository.GetStudentByNameAsync("Mock", "Interface");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Mock", result!.FirstName);
+        Assert.Equal("Interface", result!.LastName);
+    }
+}
+
---
+++ StudentApp/StudentApp/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
