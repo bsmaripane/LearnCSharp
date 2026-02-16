9208b8cf6936f8329c2db3f7c536eddd2de94165
Belmy Maripane <bsmaripane@gmail.com>
2026-02-16 08:17:11 +0200

    Refactor to use EventHandler<T> for temperature events
    Use the EventArgs and EventHandler
    Replaced custom delegate with EventHandler<TemperatureChangedEventArgs> for temperature change notifications. Introduced TemperatureChangedEventArgs to encapsulate temperature data. Updated event subscription and handler signatures throughout the application to use the new event pattern, ensuring events are only raised on actual temperature changes.

--- Section_11_TemperatureChangeEventApp/Section_11_TemperatureChangeEventApp/Program.cs
+++ Section_11_TemperatureChangeEventApp/Section_11_TemperatureChangeEventApp/Program.cs
@@ -1,49 +1,61 @@
 ﻿namespace Section_11_TemperatureChangeEventApp
 {
-    public delegate void TemperatureChangeHandler(string message);
+    // Using the Generic Delegate EventHandler<TEventArgs>
+
+    //public delegate void TemperatureChangeHandler(string message);
+
+    public class TemperatureChangedEventArgs : EventArgs
+    {
+        public int Temperature {  get; }
+
+        public TemperatureChangedEventArgs(int temperature)
+        {
+            Temperature = temperature;
+        }
+    }
 
     public class TemperatureMonitor
     {
-        public event TemperatureChangeHandler OnTemperatureChanged;
+       public event EventHandler<TemperatureChangedEventArgs> TemperatureChanged;
+
+        //public event TemperatureChangeHandler OnTemperatureChanged;
 
         private int _temperature;
 
         public int Temperature { get { return _temperature; } 
             set
             { 
-                _temperature = value;
+                
 
-                if (_temperature > 30)
+                if (_temperature != value)
                 {
+                    _temperature = value;
                     // Raise EVENT!!!
-                    RaiseTemperatureChangedEvent("Hot - Temperature is above threshold!");
-                }
-                else if (_temperature < 15)
-                {
-                    RaiseTemperatureChangedEvent("Cold - Temperature is below threshold!");
+                    OnTemperatureChanged(new TemperatureChangedEventArgs(_temperature));
                 }
             }
         }
 
-        protected virtual void RaiseTemperatureChangedEvent(string message)
+        protected virtual void OnTemperatureChanged(TemperatureChangedEventArgs e)
         {
-            OnTemperatureChanged?.Invoke(message);
+            // Letting every subscriber know!
+            TemperatureChanged?.Invoke(this, e);
         }
     }
 
     public class TemperatureAlert
     {
-        public void OnTemperatureChanged(string message)
+        public void OnTemperatureChanged(object sender, TemperatureChangedEventArgs e)
         {
-            Console.WriteLine($"Temperature Alert: {message}");
+            Console.WriteLine($"Temperature Alert: {e.Temperature} sender is {sender}");
         }
     }
 
     public class TemperatureWarning
     {
-        public void OnTemperatureChanged(string message)
+        public void OnTemperatureChanged(object sender, TemperatureChangedEventArgs e)
         {
-            Console.WriteLine($"Temperature Warning: {message}");
+            Console.WriteLine($"Temperature Warning: {e.Temperature}  sender is {sender}");
         }
     }
 
@@ -56,8 +68,8 @@
             TemperatureMonitor monitor = new TemperatureMonitor();
             TemperatureAlert alert = new TemperatureAlert();
             TemperatureWarning warning = new TemperatureWarning();
-            monitor.OnTemperatureChanged += alert.OnTemperatureChanged;
-            monitor.OnTemperatureChanged += warning.OnTemperatureChanged;
+            monitor.TemperatureChanged += alert.OnTemperatureChanged;
+            monitor.TemperatureChanged += warning.OnTemperatureChanged;
 
             monitor.Temperature = 29;
 
