26f42796a177e35a04a1ebb4c922cc2b5e4c7f46
Belmy Maripane <bsmaripane@gmail.com>
2026-02-16 15:07:43 +0200

    Create an interface, so the  app depends on behavior, not database

---
+++ StudentApp/StudentApp/Data/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentApp.Models;
+
+namespace StudentApp.Data
+{
+    internal interface IStudentRepository
+    {
+        Task AddStudentAsync(Student student);
+        Task<Student?> GetStudentByNameAsync(string firstName, string lastName);
+        Task<List<Student>> GetAllStudentsAsync();
+        Task DeleteStudentByNameAsync(string firstName, string lastName);
+    }
+}
