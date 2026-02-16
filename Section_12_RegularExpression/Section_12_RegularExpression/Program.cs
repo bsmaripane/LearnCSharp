87b2544d17caa7ed077ea3f14830b0f314d40c72
Belmy Maripane <bsmaripane@gmail.com>
2026-02-16 10:33:17 +0200

    Add regex example to find digits in a string
    Demonstrate usage of System.Text.RegularExpressions by adding code that matches and prints all digits in a sample string, including their values and positions. This provides a practical example of regular expression usage in C#.

--- Section_12_RegularExpression/Section_12_RegularExpression/Program.cs
+++ Section_12_RegularExpression/Section_12_RegularExpression/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Section_12_RegularExpression
+﻿using System.Text.RegularExpressions;
+
+namespace Section_12_RegularExpression
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Regular Expression Example");
 
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hey, my student number is 123456789";
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            Console.WriteLine("{0} hits found:\n{1}", matchCollection.Count, text);
+
+            foreach (Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+            }
 
             Console.ReadKey();
         }
