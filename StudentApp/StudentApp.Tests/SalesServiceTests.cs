b71f639ce40630923bf697b872b73a3100906e08
Belmy Maripane <bsmaripane@gmail.com>
2026-02-16 18:31:37 +0200

    Create a fake repository for sales service
    - No database
    - Pure in-memory

---
+++ StudentApp/StudentApp.Tests/SalesServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentApp.Models;
+using Xunit;
+
+namespace StudentApp.Tests
+{
+    public class SalesServiceTests
+    {
+        [Fact]
+        public async Task Should_Calculate_Total_Sales_For_Customer()
+        {
+            // Arrange
+            var fakeRepo = new FakeOrderRepository();
+
+            fakeRepo.Orders = new List<Order>
+            (
+                new Order { CustomerId = 1, Amount = 100},
+                new Order { CustomerId = 1, Amount = 200},
+                new Order { CustomerId = 2, Amount = 500}
+            );
+
+            var service = new SalesService(fakeRepo);
+
+            // Act
+            var total = await service.GetTotalSalesAsync(1);
+
+            // Assert
+            Assert.Equal(300, total);
+        }
+    }
+}
---
+++ StudentApp/StudentApp/Data/FakeOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentApp.Models;
+using StudentApp.Data;
+
+namespace StudentApp.Data
+{
+    public class FakeOrderRepository : IOrderRepository
+    {
+        public List<Order> Orders { get; set; } = new();
+
+        public Task<List<Order>> GetOrdersByCustomerAsync(int customerId)
+        {
+            var result = Orders.Where(o => o.CustomerId == customerId).ToList();
+
+            return Task.FromResult(result);
+        }
+    }
+}
