b9df68e4fabf1a85f93a773a243d0691664769da
Belmy Maripane <bsmaripane@gmail.com>
2026-02-18 08:13:42 +0200

    A car needs an engine - no dependency inject
    - The car class created its own instance of engine class
    - The car class is tightly connected to that specific engine class

---
+++ CarDependencyOnEngineApp/CarDependencyOnEngineApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CarDependencyOnEngineApp
+{
+    public class Engine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting an engine");
+        }
+    }
+
+    public class Car
+    {
+        private Engine engine = new Engine();   // Car creates the engine
+
+        public void Drive()
+        {
+            engine.Start();
+            Console.WriteLine("Car is driving");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
