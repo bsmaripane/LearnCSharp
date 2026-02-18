0bf8f7fa21597a500d84dbcec1c04e5a8a5913d8
Belmy Maripane <bsmaripane@gmail.com>
2026-02-18 11:42:16 +0200

    Create an example of the delegate
    - A delegate is a variable that can store a method
    - Create a delegate that returns void that has no parameter
    - Create a normal void method without parameter
    - Use the delegate

---
+++ DelegateExampleApp/DelegateExampleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DelegateExampleApp
+{
+    internal class Program
+    {
+        // Delegate
+        public delegate void MyDelegate();
+
+        // Method
+        public static void SayHello() { Console.WriteLine("Hello Delegate"); }
+
+        static void Main(string[] args)
+        {
+            // Use the delegate
+            MyDelegate del = SayHello;
+            del();
+
+            Console.ReadKey();
+        }
+    }
+}
