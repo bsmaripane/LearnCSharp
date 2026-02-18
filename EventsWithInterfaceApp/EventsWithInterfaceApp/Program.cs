d0e9839365e5e9531d52438399e2c8f46b55cba3
Belmy Maripane <bsmaripane@gmail.com>
2026-02-18 08:53:56 +0200

    Create an interface with event
    - Implement the interface method and event
    - Create a listener class
    - Connect them

---
+++ EventsWithInterfaceApp/EventsWithInterfaceApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace EventsWithInterfaceApp
+{
+    public interface IButton
+    {
+        event Action OnClick;
+        void Click();
+    }
+
+    public class Button : IButton
+    {
+        public event Action OnClick;
+
+        public void Click()
+        {
+            Console.WriteLine("Button clicked");
+            OnClick?.Invoke();
+        }
+    }
+
+    // Listener
+    public class Light
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Light turned on");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IButton button = new Button();
+            Light light = new Light();
+
+            button.OnClick += light.TurnOn; // raises event
+
+            button.Click();
+
+            Console.ReadKey();
+        }
+    }
+}
