124af5f4ba045363082dffd708ad4c997ae65f4d
Belmy Maripane <bsmaripane@gmail.com>
2026-02-21 14:14:19 +0200

    Create a GUI elements using  a Csharp code

---
+++ CreateGUIElementsWithCSharpWPFApp/CreateGUIElementsWithCSharpWPFApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CreateGUIElementsWithCSharpWPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
