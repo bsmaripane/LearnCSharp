2fb5e4a7bc1916416532d40ae29001515b561cdb
Belmy Maripane <bsmaripane@gmail.com>
2026-02-21 17:11:50 +0200

    Create and display invoce view user content
    - Able to change between views
    - The user control what he wants to see

--- WPF_ContentControlLoginApp/WPF_ContentControlLoginApp/InvoiceView.xaml.cs
+++ WPF_ContentControlLoginApp/WPF_ContentControlLoginApp/InvoiceView.xaml.cs
@@ -16,19 +16,13 @@ using System.Windows.Shapes;
 namespace WPF_ContentControlLoginApp
 {
     /// <summary>
-    /// Interaction logic for LoginView.xaml
+    /// Interaction logic for InvoiceView.xaml
     /// </summary>
-    public partial class LoginView : UserControl
+    public partial class InvoiceView : UserControl
     {
-        public LoginView()
+        public InvoiceView()
         {
             InitializeComponent();
-        }
-
-        private void LoginButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            Window window = Window.GetWindow(this);
-            window.Content = new InvoiceView();
         }
     }
 }
