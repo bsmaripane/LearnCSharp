1539f75832474f0bafa34a0ba006fa44ba05fa3c
Belmy Maripane <bsmaripane@gmail.com>
2026-03-03 21:57:43 +0200

    feat(car-sales-store): create the sale entity

---
+++ CarSalesStore/CarSalesStore/Sale.cs
@@ -0,0 +1,12 @@
+﻿namespace CarSalesStore
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public int CarId { get; set; }
+        public Car car { get; set; }
+        public int CustomerId { get; set; }
+        public Customer customer { get; set; }
+        public DateTime SaleDate { get; set; }
+    }
+}
