cc3e7b6ed6a8e53ff7524b38171603a44d6d0fef
Belmy Maripane <bsmaripane@gmail.com>
2026-03-03 22:08:22 +0200

    feat(car-sales-store): create the CarSalesStoreContext DbContext

---
+++ CarSalesStore/CarSalesStore/CarSalesStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarSalesStore
+{
+    public class CarSalesStoreContext
+    {
+        public CarSalesStoreContext(DbContextOptions<CarSalesStoreContext> options) : base(options) { }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+    }
+}
