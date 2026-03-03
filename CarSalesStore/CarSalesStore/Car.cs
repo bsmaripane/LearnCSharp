dbf7a1b8a101090325e0a5a1c3b6357682c1f448
Belmy Maripane <bsmaripane@gmail.com>
2026-03-03 21:48:42 +0200

    Create the car sales store project and the car class entity

---
+++ CarSalesStore/CarSalesStore/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarSalesStore
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+    }
+}
