327e8e36947e6c39156835d6cccce4965aa996a0
Belmy Maripane <bsmaripane@gmail.com>
2026-03-04 11:32:51 +0200

    fix(car-sales-store): missing non-nullable modifier ont the nullable property

--- CarSalesStore/CarSalesStore/Sale.cs
+++ CarSalesStore/CarSalesStore/Sale.cs
@@ -4,9 +4,9 @@
     {
         public int SaleId { get; set; }
         public int CarId { get; set; }
-        public Car car { get; set; }
+        public Car? car { get; set; }
         public int CustomerId { get; set; }
-        public Customer customer { get; set; }
+        public Customer? customer { get; set; }
         public DateTime SaleDate { get; set; }
     }
 }
