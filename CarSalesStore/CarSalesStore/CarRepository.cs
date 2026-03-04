1068b2882fc90f85f41a13ad44ce5ad574f84b65
Belmy Maripane <bsmaripane@gmail.com>
2026-03-04 11:27:14 +0200

    feat(repository-pattern): create CarRepository class and add the logic
    - Add the namesapce to forces compiler to use the Entity Framework Core

---
+++ CarSalesStore/CarSalesStore/CarRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarSalesStore
+{
+    public class CarRepository : ICarRepository
+    {
+        private readonly CarSalesStoreContext _context;
+
+        public CarRepository(CarSalesStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Car>> GetAllCarsAsync()
+        {
+            return await _context.Cars.ToListAsync();
+        }
+
+        public async Task<Car?> GetCarByIdAsync(int carId)
+        {
+            return await _context.Cars.FindAsync(carId);
+        }
+
+        public async Task AddCarAsync(Car car)
+        {
+            await _context.Cars.AddAsync(car);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
