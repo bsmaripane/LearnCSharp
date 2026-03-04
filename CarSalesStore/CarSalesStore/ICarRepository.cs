71218f1baa4b114626faaab7d1eb802f9ddf2a96
Belmy Maripane <bsmaripane@gmail.com>
2026-03-04 10:48:29 +0200

    feat(repository-pettern): create the logic of ICarRepository

---
+++ CarSalesStore/CarSalesStore/ICarRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CarSalesStore
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAllCarsAsync();
+        Task<Car?> GetCarByIdAsync(int carId);
+        Task AddCarAsync(Car car);
+        Task SaveChangesAsync();
+    }
+}
