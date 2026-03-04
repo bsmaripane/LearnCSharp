3ee02e2c4514673ed49d7b68256de02533e44788
Belmy Maripane <bsmaripane@gmail.com>
2026-03-04 20:25:27 +0200

    Add the cars controller

---
+++ CarSalesStore/CarSalesStore/Controllers/CarsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarSalesStore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Car API is working!");
+        }
+    }
+}
