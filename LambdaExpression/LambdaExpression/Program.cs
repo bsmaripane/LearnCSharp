58e66ce5c7ab1dbda9aad1cbb197fed8a02f9653
Belmy Maripane <bsmaripane@gmail.com>
2026-03-31 21:04:19 +0200

    test(LambdaExpression): convert to the expression tree type

--- LambdaExpression/LambdaExpression/Program.cs
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -1,15 +1,17 @@
-﻿namespace LambdaExpression
+﻿using System.Linq.Expressions;
+
+namespace LambdaExpression
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Lambda Expression!\n");
-            Console.WriteLine($"square of 5: {square(5)}");
+            Console.WriteLine($"square expression tree: {square}");
 
             Console.ReadKey();
         }
 
-        static Func<int, int> square = x => x * x;
+        static Expression<Func<int, int>> square = x => x * x;
     }
 }
