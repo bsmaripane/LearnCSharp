17265bc01393b432f2c6c74d71a80918860088cd
Belmy Maripane <bsmaripane@gmail.com>
2026-03-31 20:49:26 +0200

    test(LambdaExpression): method with one parameter and returns a value

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LambdaExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lambda Expression!\n");
+            Console.WriteLine($"square of 5: {square(5)}");
+
+            Console.ReadKey();
+        }
+
+        static Func<int, int> square = x => x * x;
+    }
+}
