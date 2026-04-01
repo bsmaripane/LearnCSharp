28276d78d2dd37244e76f50514cfdf055f8bf7bb
Belmy Maripane <bsmaripane@gmail.com>
2026-04-01 12:59:25 +0200

    test: LINQ query to get numbers greater than 5 and double them

---
+++ LINQ_GreaterThanFiveAndMultipleByTwo/LINQ_GreaterThanFiveAndMultipleByTwo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LINQ_GreaterThanFiveAndMultipleByTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 5, 7, 8, 10 };
+
+            // LINQ query to get numbers greater than 5 and double them
+            var result = numbers.Where(x => x > 5).Select(x => x * 2);
+
+            Console.WriteLine("Result of LINQ query:");
+            foreach (var number in result)
+                Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+    }
+}
