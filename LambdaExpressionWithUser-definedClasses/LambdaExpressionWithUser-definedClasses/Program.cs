75e9bd78d704b0385d296648b67432b7a9e31f0b
Belmy Maripane <bsmaripane@gmail.com>
2026-04-01 08:21:02 +0200

    feat(Student): implrment simple student class

---
+++ LambdaExpressionWithUser-definedClasses/LambdaExpressionWithUser-definedClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LambdaExpressionWithUser_definedClasses
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string StudentName { get; set; } = string.Empty;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+        }
+    }
+}
