be99c86029a737d1c92d73e017cdbe4d35b64b58
Belmy Maripane <bsmaripane@gmail.com>
2026-04-01 08:33:50 +0200

    test(LambdaExpression): sorting by student name using lambda

--- LambdaExpressionWithUser-definedClasses/LambdaExpressionWithUser-definedClasses/Program.cs
+++ LambdaExpressionWithUser-definedClasses/LambdaExpressionWithUser-definedClasses/Program.cs
@@ -10,7 +10,20 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Student> student = new List<Student>()
+            {
+                new Student{StudentId = 101, StudentName = "Maxine"},
+                new Student{StudentId = 102, StudentName = "Lesego"},
+                new Student{StudentId = 103, StudentName = "Felicia"},
+                new Student{StudentId = 104, StudentName = "Semape"},
+                new Student{StudentId = 105, StudentName = "Kganya"}
+            };
+
+            // Sorting by student name using lambda
+            var sortedList = student.OrderBy(x => x.StudentName);
+
+            foreach (var studentName in sortedList)
+                Console.WriteLine($"{studentName.StudentId} {studentName.StudentName}");
 
             Console.ReadKey();
         }
