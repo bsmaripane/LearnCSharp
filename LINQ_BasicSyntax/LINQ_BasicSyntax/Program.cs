d374725014a5dcc89f5673369919b45b5af6fc5e
Belmy Maripane <bsmaripane@gmail.com>
2026-04-03 18:19:25 +0200

    test(Basic-LINQ-Syntax): query syntax
    - from: Iterating through array of numbers
    - where: Filtering to get only even numbers
    - select: Specifying ehat to include in the final output

---
+++ LINQ_BasicSyntax/LINQ_BasicSyntax/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LINQ_BasicSyntax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenNumbers = from n in numbers
+                              where n % 2 == 0
+                              select n;
+
+            foreach (var number in evenNumbers)
+                Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+    }
+}
