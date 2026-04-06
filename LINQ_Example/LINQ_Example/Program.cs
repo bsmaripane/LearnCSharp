164d307f612041a462382fa3911f3947f8d491c3
Belmy Maripane <bsmaripane@gmail.com>
2026-04-06 17:44:53 +0200

    feat(LINQ): add print foreach loop for odd numbers

--- LINQ_Example/LINQ_Example/Program.cs
+++ LINQ_Example/LINQ_Example/Program.cs
@@ -6,6 +6,8 @@
         {
             int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+            OddNumbers(numbers);
+
             Console.ReadKey();
         }
 
@@ -14,6 +16,9 @@
             Console.WriteLine("Odd Numbers");
 
             IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+
+            foreach (int number in numbers)
+                Console.WriteLine(number);
         }
     }
 }
