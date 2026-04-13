07424e9cd579fa054b1fcda564f57ab0be1c258a
Belmy Maripane <bsmaripane@gmail.com>
2026-04-13 11:25:56 +0200

    chore(UniversityManager): add students

--- LINQ_With_Lists/UniversityManager.cs
+++ LINQ_With_Lists/UniversityManager.cs
@@ -18,6 +18,13 @@ namespace LINQ_With_Lists
             universities.Add(new University { Id = 1, Name = "UJ"});
             universities.Add(new University { Id = 2, Name = "UNISA"});
             universities.Add(new University { Id = 3, Name = "Wits"});
+
+            students.Add(new Student { Id = 1, Name = "Felicia", Gender = "Female", Age = 29, UniversityId = 1});
+            students.Add(new Student { Id = 2, Name = "Lesego", Gender = "Male", Age = 23, UniversityId = 1});
+            students.Add(new Student { Id = 3, Name = "Mmashele", Gender = "Female", Age = 53, UniversityId = 2});
+            students.Add(new Student { Id = 4, Name = "Maxine", Gender = "Female", Age = 19, UniversityId = 3});
+            students.Add(new Student { Id = 5, Name = "Kganya", Gender = "Male", Age = 17, UniversityId = 3});
+            students.Add(new Student { Id = 5, Name = "Belmy", Gender = "Male", Age = 27, UniversityId = 2});
         }
     }
 }
