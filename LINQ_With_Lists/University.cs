29c938dc2753116acf4d40b84b4dbc59355dde5c
Belmy Maripane <bsmaripane@gmail.com>
2026-04-13 10:59:22 +0200

    feat(Class): implement University class

---
+++ LINQ_With_Lists/University.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_With_Lists
+{
+    internal class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"University {Name} with id {Id}");
+        }
+    }
+}
