1f141bac191ed9342ca5c8825e9ededccaaaf04c
Belmy Maripane <bsmaripane@gmail.com>
2026-04-13 11:05:47 +0200

    feat(Student): implement Student class

---
+++ LINQ_With_Lists/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_With_Lists
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }   
+
+        // Foreign Key
+        public int UniversityId { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Student {Name} with Id {Id}, Gender {Gender} and Age {Age} from University with the Id {UniversityId}");
+        }
+    }
+}
