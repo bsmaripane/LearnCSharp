38250096fb17d9bc66d5ad18242b8e13b8829d8a
Belmy Maripane <bsmaripane@gmail.com>
2026-04-13 11:18:58 +0200

    feat(UniversityManager): implement UniversityManager class

---
+++ LINQ_With_Lists/UniversityManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_With_Lists
+{
+    internal class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        // constructor
+        public UniversityManager()
+        {
+            universities = new List<University>();
+            students = new List<Student>();
+        }
+    }
+}
