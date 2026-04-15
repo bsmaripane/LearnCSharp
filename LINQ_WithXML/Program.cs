5ed8eb23f0b042b179e366a4d2e177089902e4b0
Belmy Maripane <bsmaripane@gmail.com>
2026-04-15 15:53:40 +0200

    chore(LINQ_WithXML): Simply Student Structure to XML

---
+++ LINQ_WithXML/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LINQ_WithXML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simply Student Structure to XML
+            string studentsXML =  @"<Students>
+                                        <Student>
+                                            <Name>Felicia</Name>
+                                            <Age>29</Age>
+                                            <University>UJ</University>
+                                        </Student>
+                                        <Student>
+                                            <Name>Belicia</Name>
+                                            <Age>20</Age>
+                                            <University>Wits</University>
+                                        </Student>
+                                        <Student>
+                                            <Name>Lesego</Name>
+                                            <Age>17</Age>
+                                            <University>Wits</University>
+                                        </Student>
+                                        <Student>
+                                            <Name>Emily</Name>
+                                            <Age>54</Age>
+                                            <University>UNISA</University>
+                                        </Student>
+                                    </Students>";
+            
+            Console.ReadKey();
+        }
+    }
+}
