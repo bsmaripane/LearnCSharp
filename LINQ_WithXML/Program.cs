9b989fb1af35d4939be7e66a07bab350b50d3d43
Belmy Maripane <bsmaripane@gmail.com>
2026-04-15 16:10:32 +0200

    test(LINQ_WithXML): read XML data and print to the console

--- LINQ_WithXML/Program.cs
+++ LINQ_WithXML/Program.cs
@@ -33,7 +33,16 @@ namespace LINQ_WithXML
             XDocument studentXdoc = new XDocument();
             studentXdoc = XDocument.Parse(studentsXML);
 
+            var students = from student in studentXdoc.Descendants("Student")
+                           select new
+                           {
+                               Name = student.Element("Name").Value,
+                               Age = student.Element("Age").Value,
+                               University = student.Element("University").Value
+                           };
 
+            foreach (var student in students )
+                Console.WriteLine($"Student {student.Name} with age {student.Age} years old from University {student.University}");
             
             Console.ReadKey();
         }
