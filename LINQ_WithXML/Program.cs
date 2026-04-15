e0fecfc140dc10deee1e20c00b4ec3b14a58d0e1
Belmy Maripane <bsmaripane@gmail.com>
2026-04-15 19:20:37 +0200

    feat(LINQ_WithXML): add student course to XML and update LINQ lines

--- LINQ_WithXML/Program.cs
+++ LINQ_WithXML/Program.cs
@@ -7,25 +7,35 @@ namespace LINQ_WithXML
         static void Main(string[] args)
         {
             // Simply Student Structure to XML
-            string studentsXML =  @"<Students>
+            string studentsXML = @"<Students>
                                         <Student>
                                             <Name>Felicia</Name>
                                             <Age>29</Age>
+                                            <Course>BEd in Senior Phase and FET Teaching</Course>
                                             <University>UJ</University>
                                         </Student>
                                         <Student>
                                             <Name>Belicia</Name>
                                             <Age>20</Age>
+                                            <Course>BEng in Electrical Engineering</Course>
                                             <University>Wits</University>
                                         </Student>
                                         <Student>
                                             <Name>Lesego</Name>
                                             <Age>17</Age>
+                                            <Course>Bcom in Law</Course>
                                             <University>Wits</University>
                                         </Student>
                                         <Student>
                                             <Name>Emily</Name>
                                             <Age>54</Age>
+                                            <Course>NDip in Fasion Designer</Course>
+                                            <University>UNISA</University>
+                                        </Student>
+                                        <Student>
+                                            <Name>Emily</Name>
+                                            <Age>30</Age>
+                                            <Course>BSc in Computing</Course>
                                             <University>UNISA</University>
                                         </Student>
                                     </Students>";
@@ -38,11 +48,12 @@ namespace LINQ_WithXML
                            {
                                Name = student.Element("Name").Value,
                                Age = student.Element("Age").Value,
+                               Course = student.Element("Course").Value,
                                University = student.Element("University").Value
                            };
 
             foreach (var student in students )
-                Console.WriteLine($"Student {student.Name} with age {student.Age} years old from University {student.University}");
+                Console.WriteLine($"Student {student.Name} with age {student.Age} years old, studying {student.Course} from University {student.University}");
 
             var sortStudentsByAge = from student in students
                                     orderby student.Age
@@ -50,7 +61,7 @@ namespace LINQ_WithXML
 
             Console.WriteLine("\nSotr the list of student by age:");
             foreach (var student in sortStudentsByAge )
-                Console.WriteLine($"Student {student.Name} with age {student.Age} years old from University {student.University}");
+                Console.WriteLine($"Student {student.Name} with age {student.Age} years old, studying {student.Course} from University {student.University}");
             
             Console.ReadKey();
         }
