f1a4a5e0ce381a701067e0316c34c6e17f17c81c
Belmy Maripane <bsmaripane@gmail.com>
2026-05-31 19:03:25 +0200

    tset: uncomment all method calls

--- LinqToSQL_WPFApp1/MainWindow.xaml.cs
+++ LinqToSQL_WPFApp1/MainWindow.xaml.cs
@@ -30,9 +30,9 @@ namespace LinqToSQL_WPFApp1
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL_WPFApp1.Properties.Settings.TutorialsDbConnectionString"].ConnectionString;
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
 
-            //InsertUniversities();
-            //InsertStudents();
-            //InsertLectures();
+            InsertUniversities();
+            InsertStudents();
+            InsertLectures();
             InsertStudentLectureAssociation();
         }
 
